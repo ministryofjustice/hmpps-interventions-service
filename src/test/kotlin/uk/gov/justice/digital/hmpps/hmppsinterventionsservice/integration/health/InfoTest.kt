c05dd837762796efdf3b711e328375465cc5ce55
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-11-16 09:11:31 +0000

    run ktlintFormat

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/health/InfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/health/InfoTest.kt
@@ -11,22 +11,21 @@ class InfoTest : IntegrationTestBase() {
   @Test
   fun `Info page is accessible`() {
     webTestClient.get()
-        .uri("/info")
-        .exchange()
-        .expectStatus()
-        .isOk
-        .expectBody()
-        .jsonPath("app.name").isEqualTo("Hmpps Interventions Service")
+      .uri("/info")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("app.name").isEqualTo("Hmpps Interventions Service")
   }
 
   @Test
   fun `Info page reports version`() {
     webTestClient.get().uri("/info")
-        .exchange()
-        .expectStatus().isOk
-        .expectBody().jsonPath("build.version").value<String> {
-          assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
-        }
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("build.version").value<String> {
+        assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+      }
   }
-
 }
