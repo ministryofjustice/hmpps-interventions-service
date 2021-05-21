975cfecab08c13dfad156f49fe1aa0dab4a82d0b
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-05-21 12:09:39 +0100

    add sql definition for new mapping of service category id to complexity level id in the referral

---
+++ src/main/resources/db/migration/V1_52__cohort_complexity_levels.sql
@@ -0,0 +1,7 @@
+create table referral_complexity_level_ids (
+    referral_id uuid not null,
+    complexity_level_ids uuid,
+    complexity_level_ids_key uuid not null,
+    constraint fk_referral_complexity_level_ids_referral_id foreign key (referral_id) references referral,
+    primary key (referral_id, complexity_level_ids_key)
+);
