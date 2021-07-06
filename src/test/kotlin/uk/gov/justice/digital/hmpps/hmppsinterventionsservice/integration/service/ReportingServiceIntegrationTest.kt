99c45bcd3427aea0cb8467ae3c101dd905f6b8f1
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-07-06 16:17:29 +0100

    add integration test which validates that referrals with multiple action plans only show up once in the report

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/service/ReportingServiceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/service/ReportingServiceIntegrationTest.kt
@@ -31,15 +31,36 @@ class ReportingServiceIntegrationTest() : IntegrationTestBase() {
 
     ppUser = setupAssistant.createPPUser()
     spUser = setupAssistant.createSPUser()
-    val contract = setupAssistant.createDynamicFrameworkContract(contractReference = "PRJ_123", primeProviderId = "HARMONY_LIVING")
-    setupAssistant.createDraftReferral(intervention = setupAssistant.createIntervention(dynamicFrameworkContract = contract))
-
-    whenever(serviceProviderAccessScopeMapper.fromUser(spUser)).thenReturn(ServiceProviderAccessScope(setOf(contract.primeProvider), setOf(contract)))
   }
 
   @Test
   fun `sanity`() {
+    val contract = setupAssistant.createDynamicFrameworkContract(contractReference = "PRJ_123", primeProviderId = "HARMONY_LIVING")
+    whenever(serviceProviderAccessScopeMapper.fromUser(spUser)).thenReturn(ServiceProviderAccessScope(setOf(contract.primeProvider), setOf(contract)))
+
+    (1..10).forEach {
+      setupAssistant.createDraftReferral(intervention = setupAssistant.createIntervention(dynamicFrameworkContract = contract))
+    }
+
+    val report = reportingService.getReportData(OffsetDateTime.now().plusDays(1), OffsetDateTime.now().minusDays(1), spUser)
+    assertThat(report.size).isEqualTo(10)
+  }
+
+  @Test
+  fun `referrals with multiple action plans only show up once in the report`() {
+    val contract = setupAssistant.createDynamicFrameworkContract(contractReference = "PRJ_123", primeProviderId = "HARMONY_LIVING")
+    whenever(serviceProviderAccessScopeMapper.fromUser(spUser)).thenReturn(ServiceProviderAccessScope(setOf(contract.primeProvider), setOf(contract)))
+
+    val referral = setupAssistant.createSentReferral(intervention = setupAssistant.createIntervention(dynamicFrameworkContract = contract))
+
+    val now = OffsetDateTime.now()
+    val firstDraftActionPlan = setupAssistant.createActionPlan(referral = referral, createdAt = now.minusHours(3))
+    val firstSubmittedActionPlan = setupAssistant.createActionPlan(referral = referral, submittedAt = now.minusHours(2))
+    val firstApprovedActionPlan = setupAssistant.createActionPlan(referral = referral, submittedAt = now.minusHours(1), approvedAt = now)
+
     val report = reportingService.getReportData(OffsetDateTime.now().plusDays(1), OffsetDateTime.now().minusDays(1), spUser)
     assertThat(report.size).isEqualTo(1)
+    assertThat(report[0].dateFirstActionPlanSubmitted).isEqualTo(firstSubmittedActionPlan.submittedAt)
+    assertThat(report[0].dateOfFirstActionPlanApproval).isEqualTo(firstApprovedActionPlan.approvedAt)
   }
 }
