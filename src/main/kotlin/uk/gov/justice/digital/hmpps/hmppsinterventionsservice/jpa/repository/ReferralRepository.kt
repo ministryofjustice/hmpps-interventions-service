8c34537a4f24fadf976bd628d10cadcc302d0ff2
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-07-07 12:35:42 +0100

    fix sql to return attended session for the most recent action plan

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/ReferralRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/ReferralRepository.kt
@@ -27,79 +27,79 @@ interface ReferralRepository : JpaRepository<Referral, UUID> {
 
   @Query(
     value =
-    """WITH attended_sessions AS (
-        select count(app.id) AS attended, min(app.appointment_time) as first_appointment, aps.action_plan_id, act.referral_id
-            from appointment app 
-                join action_plan_session_appointment apsa on app.id = apsa.appointment_id
-                join action_plan_session aps on apsa.action_plan_session_id = aps.id
-                join action_plan act on aps.action_plan_id = act.id
-            where attended in ('YES', 'LATE')
-            group by aps.action_plan_id, act.referral_id),
-        outcomes_to_be_achieved AS (
-            select count(des.id) as outcomes_to_be_achieved, ref.id AS referral_id
-                from referral ref
-                    inner join referral_selected_service_category rssc on rssc.referral_id = ref.id
-                    inner join desired_outcome des on des.service_category_id = rssc.service_category_id
-                group by ref.id),
-        achieved_outcomes AS (
-            select sub.partially_achieved + sub.achieved_count AS total, referral_id
-                from (select ref.id AS referral_id,
-                    sum(case when eosro.achievement_level = 'PARTIALLY_ACHIEVED' then 0.5 end) partially_achieved,
-                    sum(case when eosro.achievement_level = 'ACHIEVED' then 1 end) achieved_count
-                        from referral ref
-                           inner join end_of_service_report eosr on eosr.referral_id = ref.id
-                           inner join end_of_service_report_outcome eosro on eosro.end_of_service_report_id = eosr.id
-                        group by ref.id) sub),
-        supplier_assessment_apointments AS (
-            select app.created_at as appointment_booked_time,
-            case when (app.attended in ('LATE', 'YES')) then app.appointment_time else null end as attended_time,
-            sup.referral_id
-                from appointment app
-                    join supplier_assessment_appointment saa on app.id = saa.appointment_id
-                    join supplier_assessment sup on saa.supplier_assessment_id = sup.id ),
-        action_plans_dates AS (
-			      select 
-                ref.id as referral_id, ap.number_of_sessions as number_of_sessions, ap.submitted_at, ap.approved_at,
-                ROW_NUMBER() OVER(PARTITION BY ref.id ORDER BY ap.approved_at ASC) as approved_at_desc_seq,
-                ROW_NUMBER() OVER(PARTITION BY ref.id ORDER BY ap.submitted_at ASC) as submitted_at_desc_seq
-			      from referral ref
-			          inner join action_plan ap on ap.referral_id = ref.id)
-        select
-            null AS referralLink,
-            ref.reference_number AS referralReference,
-            cast(ref.id as varchar) AS referralId,
-            con.contract_reference AS contractId,
-            cast(con.prime_provider_id as varchar) AS organisationId,
-            null AS referringOfficerEmail,
-            null AS caseworkerId,
-            ref.service_usercrn AS serviceUserCRN,
-            ref.created_at AS dateReferralReceived,
-            saa.appointment_booked_time AS dateSAABooked,
-            saa.attended_time AS dateSAAAttended,
-            cast(apds.submitted_at as TIMESTAMP WITH TIME ZONE) AS dateFirstActionPlanSubmitted,
-            cast(apda.approved_at as TIMESTAMP WITH TIME ZONE) AS dateOfFirstActionPlanApproval,
-            cast(ats.first_appointment as TIMESTAMP WITH TIME ZONE) AS dateOfFirstAttendedSession,
-            otba.outcomes_to_be_achieved AS outcomesToBeAchievedCount,
-            aco.total AS outcomesAchieved,
-            apda.number_of_sessions AS countOfSessionsExpected,
-            ats.attended AS countOfSessionsAttended,
-            cast(ref.end_requested_at as TIMESTAMP WITH TIME ZONE) AS endRequestedByPPAt,
-            ref.end_requested_by_id AS endRequestedByPPReason,
-            eosr.submitted_at AS dateEOSRSubmitted,
-            cast(ref.concluded_at as TIMESTAMP WITH TIME ZONE) AS concludedAt
-                from referral ref
-                    left join attended_sessions ats on ats.referral_id = ref.id
-                    left join outcomes_to_be_achieved otba on otba.referral_id = ref.id
-                    join intervention i ON (ref.intervention_id = i.id)
-                    join dynamic_framework_contract con ON (i.dynamic_framework_contract_id = con.id)
-                    left join end_of_service_report eosr ON (eosr.referral_id = ref.id)
-                    left join achieved_outcomes aco ON (aco.referral_id = ref.id)
-                    left join supplier_assessment_apointments saa on (saa.referral_id = ref.id)
-                    left join action_plans_dates apda ON (apda.referral_id = ref.id and apda.approved_at_desc_seq = 1)
-					          left join action_plans_dates apds ON (apds.referral_id = ref.id and apds.submitted_at_desc_seq = 1)
-                where (ref.sent_at >= :from or ref.sent_at is null)
-                and (ref.end_requested_at <= :to or ref.end_requested_at is null)
-                and con.id in :contracts""",
+    """with 
+action_plans_dates as (
+  	select referral_id, id action_plan_id, number_of_sessions, submitted_at, approved_at,
+		       row_number() over(partition by referral_id order by approved_at asc) as approved_at_asc_seq,
+           row_number() over(partition by referral_id order by submitted_at asc) as submitted_at_asc_seq,   
+ 		       row_number() over(partition by referral_id order by created_at desc) as created_at_desc_seq
+	  from action_plan
+),
+attended_sessions as (
+    select aps.action_plan_id, count(app.id) as attended, 
+           min(app.appointment_time) as first_appointment_time
+	  from action_plan_session aps 
+    left join action_plan_session_appointment apsa on apsa.action_plan_session_id = aps.id
+    left join appointment app on app.id = apsa.appointment_id
+    where attended in ('YES', 'LATE')
+    group by aps.action_plan_id
+),
+outcomes_to_be_achieved as (
+    select referral_id, count(service_category_id) as outcomes_count
+    from referral_selected_service_category
+    group by referral_id
+),
+achieved_outcomes as (
+	  select end_of_service_report_id,
+           sum(case when achievement_level = 'PARTIALLY_ACHIEVED' then 0.5
+                    when achievement_level = 'ACHIEVED' then 1 
+                    else 0 end) achieved_count
+    from end_of_service_report_outcome
+    group by end_of_service_report_id
+)
+select
+      null AS referralLink,
+      r.reference_number AS referralReference,
+      cast(r.id as varchar) AS referralId,
+      dfc.contract_reference AS contractId,
+      cast(dfc.prime_provider_id as varchar) AS organisationId,
+      null AS referringOfficerEmail,
+      null AS caseworkerId,
+      r.service_usercrn AS serviceUserCRN,
+      r.created_at AS dateReferralReceived,
+      sa_app.created_at AS dateSAABooked, 
+      case when (sa_app.attended in ('LATE', 'YES')) then sa_app.appointment_time else null end dateSAAAttended,
+      cast(apd_sa.submitted_at as TIMESTAMP WITH TIME ZONE) AS dateFirstActionPlanSubmitted,
+      cast(apd_aa.approved_at as TIMESTAMP WITH TIME ZONE) AS dateOfFirstActionPlanApproval,
+      cast(asess.first_appointment_time as TIMESTAMP WITH TIME ZONE) AS dateOfFirstAttendedSession,
+      otba.outcomes_count AS outcomesToBeAchievedCount,
+      oa.achieved_count AS outcomesAchieved,
+      apd_aa.number_of_sessions AS countOfSessionsExpected,
+      asess.attended AS countOfSessionsAttended,
+      cast(r.end_requested_at as TIMESTAMP WITH TIME ZONE) AS endRequestedByPPAt,
+      cr.description AS endRequestedByPPReason,
+      eosr.submitted_at AS dateEOSRSubmitted,
+      cast(r.concluded_at as TIMESTAMP WITH TIME ZONE) AS concludedAt
+from referral r
+inner join intervention i on i.id = r.intervention_id
+inner join dynamic_framework_contract dfc on dfc.id = i.dynamic_framework_contract_id
+left join outcomes_to_be_achieved otba on otba.referral_id = r.id
+left join supplier_assessment sa on sa.referral_id = r.id
+left join supplier_assessment_appointment saa on saa.supplier_assessment_id = sa.id
+left join appointment sa_app on sa_app.id = saa.appointment_id
+left join end_of_service_report eosr on eosr.referral_id = r.id 
+left join cancellation_reason cr on cr.code = r.end_requested_reason_code
+left join action_plans_dates apd_sa on apd_sa.referral_id = r.id 
+                                   and apd_sa.submitted_at_asc_seq = 1 
+left join action_plans_dates apd_aa on apd_aa.referral_id = r.id 
+                                   and apd_aa.approved_at_asc_seq = 1 
+left join action_plans_dates apd_ca on apd_ca.referral_id = r.id 
+                                   and apd_ca.created_at_desc_seq = 1
+left join attended_sessions asess on asess.action_plan_id = apd_ca.action_plan_id
+left join achieved_outcomes oa on oa.end_of_service_report_id = eosr.id
+where (r.end_requested_at >= :from or r.end_requested_at is null)
+and (r.sent_at <= :to)
+and dfc.id in :contracts""",
     nativeQuery = true
   )
   fun reportingData(from: OffsetDateTime, to: OffsetDateTime, contracts: Set<DynamicFrameworkContract>): List<ReferralReportData>
