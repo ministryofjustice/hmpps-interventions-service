5c886ec14fd622e4bfb01f7c63512ea3164ae34a
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-09-10 14:30:50 +0100

    add nullable constraints

--- src/main/resources/db/migration/V1_77_1__add_referral_data_to_action_plan_session.sql
+++ src/main/resources/db/migration/V1_77_1__add_referral_data_to_action_plan_session.sql
@@ -1,4 +1,5 @@
 UPDATE action_plan_session aps
 SET referral_id = ap.referral_id
 FROM action_plan ap
-WHERE aps.action_plan_id = ap.id;
+WHERE aps.action_plan_id = ap.id
+AND aps.referral_id is null;
--- src/main/resources/db/migration/V1_77_2__associate_action_plan_sessions_with_referrals_schema.sql
+++ src/main/resources/db/migration/V1_77_2__associate_action_plan_sessions_with_referrals_schema.sql
@@ -1,2 +1,8 @@
 alter table action_plan_session
+    alter column action_plan_id drop not null;
+
+alter table action_plan_session
     rename column action_plan_id to deprecated_action_plan_id;
+
+alter table action_plan_session
+    alter column referral_id set not null;
