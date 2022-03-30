83eb2bf2dbc512dfa3dcb9aa0ffd8b589b89a81f
kth13 <kumar.thangavel@gmail.com>
2022-03-30 16:46:26 +0100

    Added TODO to change to projection when it is ready by Spring JPA

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/ReferralForDashboardRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/ReferralForDashboardRepository.kt
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.repository.NoRepositoryBean
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ReferralForDashboard
 import java.util.UUID
 
 interface ReferralForDashboardRepository : JpaRepository<ReferralForDashboard, UUID>, JpaSpecificationExecutor<ReferralForDashboard> {
+  //TODO- Projection with specification is not been implemented by Spring JPA yet.So have to come up with this approach of creating
+  // a repository for projecting the fields we wanted. Will have to move to projection when that is ready.
   @EntityGraph(value = "entity-referral-graph")
   override fun findAll(spec: Specification<ReferralForDashboard>): List<ReferralForDashboard>
 }
