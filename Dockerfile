ministryofjustice/hmpps-interventions-service

commit 2c7d77a34e94932f64126094759ca2e07c06aafa

Leverage docker layer caching for our docker builds
`ADD . .` copies all files, including `.git/` directory

This forces `./gradlew clean assemble` to always be a fresh build on all
commits

Since we have docker layer caching on, we can split this into 3 steps:

1. download gradle and most dependencies
2. compile the runtime classes
3. parse .git information and create the JAR file

Each step only copies the relevant files, making it possible to cache
the `./gradlew` layer

The `.git` folder changes on every single file change, which means we
have to ignore `generateGitProperties` task. We only copy git files
when it is the least costly thing to do

Dockerfile
@@ -1,12 +1,26 @@
 FROM openjdk:11-slim AS builder
 
-ARG BUILD_NUMBER
-ENV BUILD_NUMBER ${BUILD_NUMBER:-1_0_0}
-
 WORKDIR /app
-ADD . .
-RUN ./gradlew clean assemble -Dorg.gradle.daemon=false
 
+ENV GRADLE_OPTS="-Dorg.gradle.daemon=false"
+
+# download most dependencies
+# exclude generateGitProperties -- .git folder is not copied to allow caching
+COPY build.gradle.kts settings.gradle.kts gradlew ./
+COPY gradle/ gradle/
+RUN ./gradlew classes --exclude-task=generateGitProperties
+
+# compile main app
+# exclude generateGitProperties -- .git folder is not copied to allow caching
+COPY src/main/ src/main/
+RUN ./gradlew classes --exclude-task=generateGitProperties
+
+# assemble extracts information from .git, this layer changes for all commits
+COPY . .
+RUN ./gradlew assemble
+
+
+# ---
 FROM adoptopenjdk/openjdk11:alpine-jre
 LABEL maintainer="HMPPS Digital Studio <info@digital.justice.gov.uk>"
 
@@ -28,7 +42,7 @@ RUN addgroup --gid 2000 --system appgroup && \
 WORKDIR /app
 COPY --from=builder --chown=appuser:appgroup /app/build/libs/hmpps-interventions-service*.jar /app/app.jar
 COPY --from=builder --chown=appuser:appgroup /app/build/libs/applicationinsights-agent*.jar /app/agent.jar
-COPY --from=builder --chown=appuser:appgroup /app/applicationinsights.json /app
+COPY --chown=appuser:appgroup applicationinsights.json /app
 
 USER 2000
 

commit ce0c3995c81074eece97ae87314e8dfba0aaf445

🐛 Build with full version number
2c7d77a34e94932f64126094759ca2e07c06aafa introduced a regression in the
application version -- the built version is `YYYY-MM-DD` now instead of
`YYYY-MM-DD.buildNumber.gitSHA`

This is because `ARG BUILD_NUMBER` was removed, which is used by the
gradle task `bootBuildInfo` to pass it in

Dockerfile
@@ -15,7 +15,9 @@ RUN ./gradlew classes --exclude-task=generateGitProperties
 COPY src/main/ src/main/
 RUN ./gradlew classes --exclude-task=generateGitProperties
 
-# assemble extracts information from .git, this layer changes for all commits
+# assemble extracts information from .git and BUILD_NUMBER env var, these layers change for all commits
+ARG BUILD_NUMBER
+ENV BUILD_NUMBER ${BUILD_NUMBER:-1_0_0}
 COPY . .
 RUN ./gradlew assemble
 