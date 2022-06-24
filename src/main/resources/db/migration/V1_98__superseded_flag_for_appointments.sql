6a7e4eed89b46aa4019728837cc1d44339844659
kth13 <kumar.thangavel@gmail.com>
2022-06-24 14:05:03 +0100

    amended some comments

--- src/main/resources/db/migration/V1_98__superseded_flag_for_appointments.sql
+++ src/main/resources/db/migration/V1_98__superseded_flag_for_appointments.sql
@@ -2,9 +2,10 @@
 ALTER TABLE appointment
     ADD COLUMN superseded bool NOT NULL DEFAULT false;
 
--- migrating the values to "true" will be done in when a new reschedule start to happen
--- • all the appointments can be false to start with
--- • reschedule appointments happens, new appointments will be created instead of overwriting and the old appointments will be set to true
+-- migrating the values to "true" will be done when rescheduling an appointment
+-- all the appointments can be false to start with
+-- reschedule appointments happens, new appointments will be created and instead of overwriting to the old appointments.
+-- the old appointments will be set to true
 
 -- inserting the metadata for the new column
 INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','superseded',FALSE, TRUE);
