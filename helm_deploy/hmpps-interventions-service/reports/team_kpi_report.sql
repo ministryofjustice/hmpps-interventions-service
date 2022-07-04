2b5b031b2b6a8b0877eed02d83daa8cc936002be
David Lantos <david.lantos@gmail.com>
2022-07-04 10:03:11 +0100

    Replace mistaken referrals with completed referrals
    "Completed" meaning all it concluded naturally, without the probation
    practitioner requesting an early end.
    
    "Mistaken" referrals are not very useful here.

--- helm_deploy/hmpps-interventions-service/reports/team_kpi_report.sql
+++ helm_deploy/hmpps-interventions-service/reports/team_kpi_report.sql
@@ -1,16 +1,17 @@
-WITH counts AS (SELECT COUNT(r.id)                                                      AS count_of_started_referrals,
-                       COUNT(r.id) FILTER ( WHERE r.sent_at IS NULL )                   AS count_of_draft_referrals,
-                       COUNT(r.id) FILTER ( WHERE r.sent_at IS NOT NULL )               AS count_of_sent_referrals,
-                       COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL )          AS count_of_concluded_referrals,
+WITH counts AS (SELECT COUNT(r.id)                                                AS count_of_started_referrals,
+                       COUNT(r.id) FILTER ( WHERE r.sent_at IS NULL )             AS count_of_draft_referrals,
+                       COUNT(r.id) FILTER ( WHERE r.sent_at IS NOT NULL )         AS count_of_sent_referrals,
                        COUNT(r.id) FILTER ( WHERE r.sent_at IS NOT NULL AND
-                                                  r.concluded_at IS NULL )              AS count_of_live_referrals,
+                                                  r.concluded_at IS NULL )        AS count_of_live_referrals,
                        COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL AND
                                                   r.end_requested_at IS NOT NULL AND
-                                                  eosr.submitted_at IS NULL )           AS count_of_cancelled_referrals,
+                                                  eosr.submitted_at IS NULL )     AS count_of_cancelled_referrals,
                        COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL AND
                                                   r.end_requested_at IS NOT NULL AND
-                                                  eosr.submitted_at IS NOT NULL )       AS count_of_early_end_referrals,
-                       COUNT(r.id) FILTER ( WHERE r.end_requested_reason_code = 'MIS' ) AS count_of_mistaken_referrals
+                                                  eosr.submitted_at IS NOT NULL ) AS count_of_early_end_referrals,
+                       COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL AND
+                                                  r.end_requested_at IS NULL )    AS count_of_completed_referrals,
+                       COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL )    AS count_of_concluded_referrals
                 FROM referral r
                          LEFT JOIN end_of_service_report eosr ON r.id = eosr.referral_id),
 
@@ -23,11 +24,11 @@ WITH counts AS (SELECT COUNT(r.id)                                              
 SELECT counts.count_of_started_referrals,
        counts.count_of_draft_referrals,
        counts.count_of_sent_referrals,
-       counts.count_of_concluded_referrals,
        counts.count_of_live_referrals,
        counts.count_of_cancelled_referrals,
-       counts.count_of_mistaken_referrals,
        counts.count_of_early_end_referrals,
+       counts.count_of_completed_referrals,
+       counts.count_of_concluded_referrals,
        times.p50_completion_time,
        times.p95_completion_time,
        times.p99_completion_time
