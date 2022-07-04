f19f1ab387bf627387e7bb378c6e3819bf4448d0
David Lantos <david.lantos@gmail.com>
2022-07-04 10:13:46 +0100

    Limit results to the previous 7 days

--- helm_deploy/hmpps-interventions-service/reports/team_kpi_report.sql
+++ helm_deploy/hmpps-interventions-service/reports/team_kpi_report.sql
@@ -1,4 +1,7 @@
-WITH counts AS (SELECT COUNT(r.id)                                                AS count_of_started_referrals,
+WITH dates AS (SELECT (DATE_TRUNC('day', NOW()) - '7 days'::interval) AS start_date,
+                      DATE_TRUNC('day', NOW())                        AS end_date),
+
+     counts AS (SELECT COUNT(r.id)                                                AS count_of_started_referrals,
                        COUNT(r.id) FILTER ( WHERE r.sent_at IS NULL )             AS count_of_draft_referrals,
                        COUNT(r.id) FILTER ( WHERE r.sent_at IS NOT NULL )         AS count_of_sent_referrals,
                        COUNT(r.id) FILTER ( WHERE r.sent_at IS NOT NULL AND
@@ -13,15 +16,21 @@ WITH counts AS (SELECT COUNT(r.id)                                              
                                                   r.end_requested_at IS NULL )    AS count_of_completed_referrals,
                        COUNT(r.id) FILTER ( WHERE r.concluded_at IS NOT NULL )    AS count_of_concluded_referrals
                 FROM referral r
-                         LEFT JOIN end_of_service_report eosr ON r.id = eosr.referral_id),
+                         LEFT JOIN end_of_service_report eosr ON r.id = eosr.referral_id,
+                     dates d
+                WHERE COALESCE(r.concluded_at, r.end_requested_at, r.sent_at, r.created_at) BETWEEN d.start_date AND d.end_date),
 
      times AS (SELECT PERCENTILE_CONT(0.50) WITHIN GROUP ( ORDER BY sent_at - created_at ) AS p50_completion_time,
                       PERCENTILE_CONT(0.95) WITHIN GROUP ( ORDER BY sent_at - created_at ) AS p95_completion_time,
                       PERCENTILE_CONT(0.99) WITHIN GROUP ( ORDER BY sent_at - created_at ) AS p99_completion_time
-               FROM referral
-               WHERE sent_at IS NOT NULL)
+               FROM referral r,
+                    dates d
+               WHERE sent_at IS NOT NULL
+                 AND COALESCE(r.concluded_at, r.end_requested_at, r.sent_at, r.created_at) BETWEEN d.start_date AND d.end_date)
 
-SELECT counts.count_of_started_referrals,
+SELECT dates.start_date::date   AS start_date_inclusive,
+       dates.end_date::date - 1 AS end_date_inclusive,
+       counts.count_of_started_referrals,
        counts.count_of_draft_referrals,
        counts.count_of_sent_referrals,
        counts.count_of_live_referrals,
@@ -33,5 +42,6 @@ SELECT counts.count_of_started_referrals,
        times.p95_completion_time,
        times.p99_completion_time
 
-FROM counts,
+FROM dates,
+     counts,
      times;
