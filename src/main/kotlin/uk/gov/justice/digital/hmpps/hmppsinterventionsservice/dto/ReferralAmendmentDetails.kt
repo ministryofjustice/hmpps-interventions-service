43da43e64a08250951f0070baf915137cf1b9db3
kth13 <kumar.thangavel@gmail.com>
2022-07-29 10:03:12 +0100

    Revert "Added back-end changes to amend a referral (complexity level)"
    This reverts commit 7ab4207a

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/ReferralAmendmentDetails.kt
+++
@@ -1,5 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto
-
-data class ReferralAmendmentDetails(
-  var complexityLevelIds: List<String>
-)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/ChangelogRepository.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Changelog
-import java.util.UUID
-
-interface ChangelogRepository : JpaRepository<Changelog, UUID>
