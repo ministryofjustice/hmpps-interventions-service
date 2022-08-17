6df5d5a7edd5d39c8d1be3f86589dbf8fcf90d0a
markmoj <mark.evans@digital.justice.gov.uk>
2022-08-17 13:38:55 +0100

    Addtiinal Amend types

--- src/main/resources/db/migration/V1_101_1__alter_changelog_table.sql
+++ src/main/resources/db/migration/V1_101_1__alter_changelog_table.sql
@@ -1,6 +1,3 @@
-CREATE TYPE amendTypes AS ENUM ('COMPLEXITY_LEVEL',
-                                'DESIRED_OUTCOMES',
-                                'COMPLETION_DATETIME',
-                                'MAXIMUM_ENFORCEABLE_DAYS');
+CREATE TYPE amendTypes AS ENUM ('COMPLEXITY_LEVEL','DESIRED_OUTCOMES');
 
 alter table changelog alter column topic TYPE amendTypes using topic::amendTypes;
