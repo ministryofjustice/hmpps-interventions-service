efe5296e3d9e306bd9418ff271dadf3c5853240b
OmotayoTalabi <omotayo.talabi@digital.justice.gov.uk>
2022-09-06 18:03:42 +0100

    Revert "to fix date on change log"
    This reverts commit 10bd5abc175854d5087386b9784cd636175e1653.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/controller/AmendReferralControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/controller/AmendReferralControllerTest.kt
@@ -228,6 +228,7 @@ internal class AmendReferralControllerTest {
       assertThat(returnedChangeLogObject?.size).isEqualTo(2)
       assertThat(returnedChangeLogObject?.get(0)?.changelogId).isEqualTo(changeLogValuesList.get(0).id)
       assertThat(returnedChangeLogObject?.get(0)?.referralId).isEqualTo(changeLogValuesList.get(0).referralId)
+
     }
 
     @Test
