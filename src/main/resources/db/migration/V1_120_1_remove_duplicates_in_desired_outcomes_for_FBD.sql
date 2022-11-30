470781a115b5013018deed0c959a618ccede4aef
kth13 <kumar.thangavel@gmail.com>
2022-11-30 15:16:51 +0000

    correct duplicates in FBD NW

---
+++ src/main/resources/db/migration/V1_120_1_remove_duplicates_in_desired_outcomes_for_FBD.sql
@@ -0,0 +1,4 @@
+DELETE from desired_outcome where id in ('af1d6c9c-bcdd-4e9f-a778-5c1a1e90163c', 'ff927255-28df-4a1d-8076-9ce18d959442');
+
+update desired_outcome set description = 'The person referred can successfully navigate the benefits system' where id = '39393e5f-48d2-4981-84d1-e23d762f65b8';
+update desired_outcome set description = 'The person referred’s financial management skills are developed and/or enhanced, including online banking skills' where id = 'a0e18ea2-9982-43c5-9155-8a07ec88fe75';
