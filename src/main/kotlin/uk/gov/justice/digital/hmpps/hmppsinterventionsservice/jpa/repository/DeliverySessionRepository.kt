8055313122dc7e52c67616200f2c0c06afc4b42c
David Lantos <david.lantos@gmail.com>
2022-12-29 15:42:56 +0000

    Deprecate action plan usage in delivery sessions
    Our initial data models were using a referral → actionPlan → sessions
    chain.
    
    If I recall correctly, when we implemented action plan revisions, it
    enabled "extending" the number of sessions, so we had to tie the session
    counts to the referral instead.
    
    This means all of the `actionPlanId` looks are deprecated and should be
    removed.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/DeliverySessionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/repository/DeliverySessionRepository.kt
@@ -9,9 +9,17 @@ interface DeliverySessionRepository : JpaRepository<DeliverySession, UUID> {
   fun findAllByReferralId(referralId: UUID): List<DeliverySession>
   fun findByReferralIdAndSessionNumber(referralId: UUID, sessionNumber: Int): DeliverySession?
 
+  @Deprecated(
+    "looking up by action plan ID is no longer necessary",
+    ReplaceWith("findByReferralIdAndSessionNumber(referralId, sessionNumber")
+  )
   @Query("select aps from DeliverySession aps left join ActionPlan ap on ap.referral = aps.referral where ap.id = :actionPlanId and aps.sessionNumber = :sessionNumber")
   fun findAllByActionPlanIdAndSessionNumber(actionPlanId: UUID, sessionNumber: Int): DeliverySession?
 
+  @Deprecated(
+    "looking up by action plan ID is no longer necessary",
+    ReplaceWith("findAllByReferralId(referralId")
+  )
   @Query("select ds from DeliverySession ds left join ActionPlan ap on ap.referral = ds.referral where ap.id = :actionPlanId")
   fun findAllByActionPlanId(actionPlanId: UUID): List<DeliverySession>
 }
