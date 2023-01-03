16d1521baa038228f5f2c32c6b22660f474f47ca
David Lantos <david.lantos@gmail.com>
2023-01-03 12:20:48 +0000

    Refactor to one file per event structure
    So that all listeners for the same event are in the same file.
    This follows a newer work's pattern with referral end listeners.
    
    If this grows unwieldy, we can put them in the same package.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/EndOfServiceReportListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/EndOfServiceReportListener.kt
@@ -1,16 +1,39 @@
-package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.notifications
+package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.listeners
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.EmailSender
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.SNSPublisher
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.EventDTO
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.EndOfServiceReportSubmittedEvent
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.exception.AsyncEventExceptionHandling
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.NotifyService
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.ReferralService
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.SNSService
 
 @Service
-class NotifyEndOfServiceReportListener(
+class EndOfServiceReportDomainEventListener(
+  private val snsPublisher: SNSPublisher
+) : ApplicationListener<EndOfServiceReportSubmittedEvent>, SNSService {
+  @AsyncEventExceptionHandling
+  override fun onApplicationEvent(event: EndOfServiceReportSubmittedEvent) {
+    val snsEvent = EventDTO(
+      "intervention.end-of-service-report.submitted",
+      "An end of service report has been submitted",
+      event.detailUrl,
+      event.submittedAt,
+      mapOf(
+        "endOfServiceReportId" to event.endOfServiceReportId.toString(),
+        "submittedBy" to event.submittedBy.userName
+      )
+    )
+    snsPublisher.publish(event.referralId, event.submittedBy, snsEvent)
+  }
+}
+
+@Service
+class EndOfServiceReportNotificationListener(
   @Value("\${notify.templates.end-of-service-report-submitted}") private val endOfServiceReportSubmittedTemplateID: String,
   @Value("\${interventions-ui.baseurl}") private val interventionsUIBaseURL: String,
   @Value("\${interventions-ui.locations.probation-practitioner.end-of-service-report}") private val ppEndOfServiceReportLocation: String,
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/EndOfServiceReportListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/EndOfServiceReportListenerTest.kt
@@ -1,9 +1,8 @@
-package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.notifications
+package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.listeners
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.mockito.kotlin.any
 import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
@@ -11,6 +10,8 @@ import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.EmailSender
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.SNSPublisher
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.EventDTO
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.EndOfServiceReportSubmittedEvent
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.ReferralService
@@ -19,26 +20,49 @@ import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ReferralFacto
 import java.time.OffsetDateTime
 import java.util.UUID
 
-class NotifyEndOfServiceReportListenerTest {
+private val now = OffsetDateTime.now()
+private val submittedEvent = EndOfServiceReportSubmittedEvent(
+  source = "source",
+  referralId = UUID.fromString("56b40f96-0657-4e01-925c-da208a6fbcfd"),
+  detailUrl = "https://detail-url.example.org/eosr/id",
+  endOfServiceReportId = UUID.fromString("f5033a88-a71d-4b8a-b2e1-b80e749f6d2c"),
+  submittedAt = now,
+  submittedBy = AuthUser("eosrSubmitterId", "eosrSubmitterSource", "eosrSubmitterUserName")
+)
+
+internal class EndOfServiceReportDomainEventListenerTest {
+  private val publisher = mock<SNSPublisher>()
+  private val endOfServiceReportDomainEventListener = EndOfServiceReportDomainEventListener(publisher)
+
+  @Test
+  fun `publishes domain event message when an end of service report is submitted`() {
+    endOfServiceReportDomainEventListener.onApplicationEvent(submittedEvent)
+
+    val eventDTO = EventDTO(
+      eventType = "intervention.end-of-service-report.submitted",
+      description = "An end of service report has been submitted",
+      detailUrl = "https://detail-url.example.org/eosr/id",
+      occurredAt = now,
+      additionalInformation = mapOf(
+        "endOfServiceReportId" to "f5033a88-a71d-4b8a-b2e1-b80e749f6d2c",
+        "submittedBy" to "eosrSubmitterUserName"
+      )
+    )
+
+    verify(publisher).publish(submittedEvent.referralId, submittedEvent.submittedBy, eventDTO)
+  }
+}
+
+internal class EndOfServiceReportNotificationListenerTest {
   private val emailSender = mock<EmailSender>()
   private val referralService = mock<ReferralService>()
   private val referralFactory = ReferralFactory()
-  private val now = OffsetDateTime.now()
 
-  private val referral = referralFactory.createSent()
+  private val referral = referralFactory.createSent(id = submittedEvent.referralId)
 
-  private val submittedEvent = EndOfServiceReportSubmittedEvent(
-    source = "source",
-    referralId = referral.id,
-    detailUrl = "https://detail-url.example.org/eosr/id",
-    endOfServiceReportId = UUID.fromString("f5033a88-a71d-4b8a-b2e1-b80e749f6d2c"),
-    submittedAt = now,
-    submittedBy = AuthUser("eosrSubmitterId", "eosrSubmitterSource", "eosrSubmitterUserName")
-  )
-
-  private fun notifyService(): NotifyEndOfServiceReportListener {
+  private fun notifyService(): EndOfServiceReportNotificationListener {
     whenever(referralService.getSentReferral(referral.id)).thenReturn(referral)
-    return NotifyEndOfServiceReportListener(
+    return EndOfServiceReportNotificationListener(
       "template",
       "http://example.com",
       "/end-of-service-report/{id}",
@@ -49,7 +73,7 @@ class NotifyEndOfServiceReportListenerTest {
 
   @Test
   fun `end of service report submitted event does not send email when user details are not available`() {
-    whenever(referralService.getResponsibleProbationPractitioner(any())).thenThrow(RuntimeException::class.java)
+    whenever(referralService.getResponsibleProbationPractitioner(referral)).thenThrow(RuntimeException::class.java)
     assertThrows<RuntimeException> {
       notifyService().onApplicationEvent(submittedEvent)
     }
@@ -58,7 +82,7 @@ class NotifyEndOfServiceReportListenerTest {
 
   @Test
   fun `end of service report submitted event generates valid url and sends an email`() {
-    whenever(referralService.getResponsibleProbationPractitioner(any()))
+    whenever(referralService.getResponsibleProbationPractitioner(referral))
       .thenReturn(ResponsibleProbationPractitioner("abc", "abc@abc.abc", null, null, "def"))
 
     notifyService().onApplicationEvent(submittedEvent)
