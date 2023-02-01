c70ca5cfd3844794dfeccf02823120302fee7f37
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-02-01 16:33:59 +0000

    Fix for failing metadata checks
    Amend metadata to match column names for draft referral table new columns.

--- src/main/resources/db/migration/V1_125__add_expected_release_date.sql
+++ src/main/resources/db/migration/V1_125__add_expected_release_date.sql
@@ -6,8 +6,8 @@ alter table referral_location
     add column expected_release_date date,
     add column expected_release_date_missing_reason text;
 
-INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('draft_referral','person_current_expected_release_date',TRUE, TRUE);
-INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('draft_referral','person_custody_expected_release_date_missing_reason',TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('draft_referral','person_expected_release_date',TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('draft_referral','person_expected_release_date_missing_reason',TRUE, TRUE);
 
 INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('referral_location','expected_release_date',TRUE, TRUE);
 INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('referral_location','expected_release_date_missing_reason',TRUE, TRUE);
