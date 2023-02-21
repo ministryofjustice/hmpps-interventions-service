f9394b0ed055432c4f968e78824e5003b722e4f5
David Lantos <david.lantos@gmail.com>
2023-02-21 22:42:49 +0000

    Emit domain event before terminating NSIs
    I still need to add `personReferences` to the domain event payload, but
    that requires wider changes (in next commits).

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/ReferralEndingListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/ReferralEndingListener.kt
@@ -5,7 +5,10 @@ import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Service
 import org.springframework.web.util.UriComponentsBuilder
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.CommunityAPIClient
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.SNSPublisher
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.EventDTO
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEndingEvent
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.CommunityAPIService
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.ReferralEndRequest
 import java.time.OffsetDateTime
@@ -17,9 +20,31 @@ class ReferralEndingListener(
   @Value("\${interventions-ui.locations.probation-practitioner.referral-details}") private val ppReferralDetailsLocation: String,
   @Value("\${community-api.locations.ended-referral}") private val communityAPIEndedReferralLocation: String,
   @Value("\${community-api.integration-context}") private val integrationContext: String,
+  private val snsPublisher: SNSPublisher,
   private val communityAPIClient: CommunityAPIClient,
 ) : ApplicationListener<ReferralEndingEvent>, CommunityAPIService {
   override fun onApplicationEvent(event: ReferralEndingEvent) {
+    sendDomainEvent(event)
+    terminateNSI(event)
+  }
+
+  private fun sendDomainEvent(event: ReferralEndingEvent) {
+    val snsEvent = EventDTO(
+      eventType = "intervention.referral.ended",
+      description = "The referral has ended. It might still require admin work from providers",
+      detailUrl = event.detailUrl,
+      occurredAt = endedTime(event),
+      additionalInformation = mapOf(
+        "deliveryState" to event.state.name,
+        "referralId" to event.referral.id.toString(),
+        "referralURN" to event.referral.urn,
+        "referralProbationUserURL" to referralDetailsUrl(event.referral.id),
+      ),
+    )
+    snsPublisher.publish(event.referral.id, AuthUser.interventionsServiceUser, snsEvent)
+  }
+
+  private fun terminateNSI(event: ReferralEndingEvent) {
     communityAPIClient.makeAsyncPostRequest(
       communityAPISentReferralUrl(event.referral.serviceUserCRN),
       ReferralEndRequest(
@@ -34,6 +59,8 @@ class ReferralEndingListener(
     )
   }
 
+  // TODO code smell: we are guessing at what happened by looking at data
+  // Better would be to emit "ReferralCancelledEvent" and "ReferralCompletedEvent" and listen to them to create this domain event
   private fun endedTime(event: ReferralEndingEvent): OffsetDateTime =
     event.referral.concludedAt ?: event.referral.endRequestedAt ?: OffsetDateTime.now()
 
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/ReferralEndingListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/listeners/ReferralEndingListenerTest.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.listeners
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.CommunityAPIClient
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.SNSPublisher
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.EventDTO
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEndingEvent
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ReferralAssignment
@@ -16,7 +19,7 @@ import java.time.ZoneOffset
 import java.util.UUID
 
 private val sentAtDefault = OffsetDateTime.of(2020, 1, 1, 1, 1, 1, 1, ZoneOffset.UTC)
-private val concludedAtDefault = OffsetDateTime.of(2020, 2, 2, 2, 2, 2, 2, ZoneOffset.UTC)
+private val cancelledAtDefault = OffsetDateTime.of(2020, 2, 2, 2, 2, 2, 2, ZoneOffset.UTC)
 
 private fun referralEndingEvent(state: ReferralConcludedState): ReferralEndingEvent {
   val referralFactory = ReferralFactory()
@@ -29,7 +32,7 @@ private fun referralEndingEvent(state: ReferralConcludedState): ReferralEndingEv
       referenceNumber = "HAS71263",
       relevantSentenceId = 123456789,
       sentAt = sentAtDefault,
-      concludedAt = concludedAtDefault,
+      endRequestedAt = cancelledAtDefault,
       endRequestedBy = AuthUser("ecd7b8d690", "irrelevant", "irrelevant"),
       assignments = listOf(
         ReferralAssignment(
@@ -44,70 +47,54 @@ private fun referralEndingEvent(state: ReferralConcludedState): ReferralEndingEv
 }
 
 internal class ReferralEndingListenerTest {
+  private val snsPublisher = mock<SNSPublisher>()
   private val communityAPIClient = mock<CommunityAPIClient>()
   private val listener = ReferralEndingListener(
     "http://testUrl",
     "/pp/referral/{id}",
     "secure/offenders/crn/{crn}/referral/end/context/{contextName}",
     "commissioned-rehabilitation-services",
-    communityAPIClient
+    snsPublisher,
+    communityAPIClient,
   )
 
-  @Test
-  fun `terminates the NSI for the cancelled referral`() {
-    val event = referralEndingEvent(ReferralConcludedState.CANCELLED)
+  @ParameterizedTest
+  @EnumSource(ReferralConcludedState::class)
+  fun `publishes domain event`(state: ReferralConcludedState) {
+    val event = referralEndingEvent(state)
     listener.onApplicationEvent(event)
 
-    verify(communityAPIClient).makeAsyncPostRequest(
-      "secure/offenders/crn/X123456/referral/end/context/commissioned-rehabilitation-services",
-      ReferralEndRequest(
-        "ACC",
-        sentAtDefault,
-        concludedAtDefault,
-        123456789,
-        event.referral.id,
-        "CANCELLED",
-        "Referral Ended for Accommodation Referral HAS71263 with Prime Provider Harmony Living\n" +
-          "http://testUrl/pp/referral/68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
+    val eventPayload = EventDTO(
+      eventType = "intervention.referral.ended",
+      description = "The referral has ended. It might still require admin work from providers",
+      detailUrl = "http://localhost:8080/sent-referral/68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
+      occurredAt = cancelledAtDefault,
+      additionalInformation = mapOf(
+        "deliveryState" to state.name,
+        "referralId" to "68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
+        "referralURN" to "urn:hmpps:interventions-referral:68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
+        "referralProbationUserURL" to "http://testUrl/pp/referral/68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
       )
     )
+    verify(snsPublisher).publish(event.referral.id, AuthUser.interventionsServiceUser, eventPayload)
   }
 
-  @Test
-  fun `terminates the NSI for the prematurely ended referral`() {
-    val event = referralEndingEvent(ReferralConcludedState.PREMATURELY_ENDED)
+  @ParameterizedTest
+  @EnumSource(ReferralConcludedState::class)
+  fun `terminates the NSI`(state: ReferralConcludedState) {
+    val event = referralEndingEvent(state)
     listener.onApplicationEvent(event)
 
     verify(communityAPIClient).makeAsyncPostRequest(
       "secure/offenders/crn/X123456/referral/end/context/commissioned-rehabilitation-services",
       ReferralEndRequest(
-        "ACC",
-        sentAtDefault,
-        concludedAtDefault,
-        123456789,
-        event.referral.id,
-        "PREMATURELY_ENDED",
-        "Referral Ended for Accommodation Referral HAS71263 with Prime Provider Harmony Living\n" +
-          "http://testUrl/pp/referral/68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
-      )
-    )
-  }
-
-  @Test
-  fun `terminates the NSI for the completed referral`() {
-    val event = referralEndingEvent(ReferralConcludedState.COMPLETED)
-    listener.onApplicationEvent(event)
-
-    verify(communityAPIClient).makeAsyncPostRequest(
-      "secure/offenders/crn/X123456/referral/end/context/commissioned-rehabilitation-services",
-      ReferralEndRequest(
-        "ACC",
-        sentAtDefault,
-        concludedAtDefault,
-        123456789,
-        event.referral.id,
-        "COMPLETED",
-        "Referral Ended for Accommodation Referral HAS71263 with Prime Provider Harmony Living\n" +
+        contractType = "ACC",
+        startedAt = sentAtDefault,
+        endedAt = cancelledAtDefault,
+        sentenceId = 123456789,
+        referralId = event.referral.id,
+        endType = state.name,
+        notes = "Referral Ended for Accommodation Referral HAS71263 with Prime Provider Harmony Living\n" +
           "http://testUrl/pp/referral/68df9f6c-3fcb-4ec6-8fcf-96551cd9b080",
       )
     )
