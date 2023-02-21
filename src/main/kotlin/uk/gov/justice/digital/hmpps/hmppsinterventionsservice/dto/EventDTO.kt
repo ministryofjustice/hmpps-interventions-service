d7b0000b196e4816498d664cbc112329c8a8c754
David Lantos <david.lantos@gmail.com>
2023-02-21 23:39:28 +0000

    Add support for person references in domain events
    Following the schema on https://dsdmoj.atlassian.net/wiki/pages/viewpage.action?pageId=3732570166&pageVersion=13

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/EventDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/EventDTO.kt
@@ -2,12 +2,26 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto
 
 import java.time.OffsetDateTime
 
+data class PersonIdentifier(
+  val type: String,
+  val value: String,
+)
+
+data class PersonReference(
+  val identifiers: List<PersonIdentifier> = listOf(),
+) {
+  companion object {
+    fun crn(crn: String): PersonReference = PersonReference(listOf(PersonIdentifier(type = "CRN", value = crn)))
+  }
+}
+
 data class EventDTO(
   val eventType: String,
   val description: String,
   val detailUrl: String,
   val occurredAt: OffsetDateTime,
-  val additionalInformation: Map<String, Any>
+  val additionalInformation: Map<String, Any>,
+  val personReference: PersonReference? = null,
 ) {
   val version: Int = 1
 }
