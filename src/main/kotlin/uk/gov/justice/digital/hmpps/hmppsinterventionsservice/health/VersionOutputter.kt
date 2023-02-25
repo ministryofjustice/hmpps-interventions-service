5c2dbf4c7e3a8435ed7fdf67b85f6e386c9b40fd
David Lantos <david.lantos@gmail.com>
2023-02-25 23:28:21 +0000

    Remove gone context initialiser from AppInsights
    I could not find any replacement.
    
    To test: if version of the application is correctly reported in
    telemetry.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/health/VersionOutputter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/health/VersionOutputter.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.health
 
-import com.microsoft.applicationinsights.extensibility.ContextInitializer
 import mu.KLogging
 import net.logstash.logback.argument.StructuredArguments.kv
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.info.BuildProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.EventListener
 
@@ -19,7 +17,4 @@ class VersionOutputter(buildProperties: BuildProperties) {
   fun logVersionOnStartup() {
     logger.info("application started {}", kv("application_version", version))
   }
-
-  @Bean
-  fun versionContextInitializer() = ContextInitializer { it.component.setVersion(version) }
 }
