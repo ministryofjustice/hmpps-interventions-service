d9532ac39620a57cc438d975aa133df99347b1b1
Marco Collura <marco.collura@digital.justice.gov.uk>
2023-03-10 11:24:18 +0000

    Add service categories to GM Women's services

---
+++ src/main/resources/db/migration/V1_133__add_gmws_service_categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO contract_type_service_category (contract_type_id, service_category_id)
+VALUES
+-- Women's Support Services (GM)
+     ('a93c152c-ed56-48f9-92e8-401ff7aa2fa8', '428ee70f-3001-4399-95a6-ad25eaaede16') -- Accommodation
+     , ('a93c152c-ed56-48f9-92e8-401ff7aa2fa8', '96a63c39-4371-4f17-a6ec-265755f0cf7b') -- Finance, Benefits and Debt
+     , ('a93c152c-ed56-48f9-92e8-401ff7aa2fa8', '76bcdb97-1dea-41c1-a4f8-899d88e5d679') -- Dependency and Recovery
