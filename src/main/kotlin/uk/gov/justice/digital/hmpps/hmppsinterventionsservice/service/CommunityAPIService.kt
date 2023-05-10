b4efe2cdce05a886a37b29bcd31bfefcac945563
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-05-10 19:29:19 +0100

    added deliusAppointmentId to api

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/CommunityAPIService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/CommunityAPIService.kt
@@ -121,7 +121,7 @@ class CommunityAPIAppointmentEventService(
     false,
     attended?.let { AppointmentMerge.Outcome(it.forMerge(), (attended == NO || notifyPPOfAttendanceBehaviour == true)) },
     null,
-    null,
+    deliusAppointmentId
   )
 
   private fun Attended.forMerge() = when (this) {
