2af70d4383f32142545a0051f01c213c1bbb0cd5
Marco Collura <marco.collura@digital.justice.gov.uk>
2023-06-16 13:52:28 +0100

    Update metadata table with new feedback fields

--- src/main/resources/db/migration/V1_139__add_appointment_session_feedback.sql
+++ src/main/resources/db/migration/V1_139__add_appointment_session_feedback.sql
@@ -7,3 +7,10 @@ alter table appointment
     add column session_feedback_submitted_at timestamp with time zone,
 
     add constraint fk_session_feedback_submitted_by_id foreign key (session_feedback_submitted_by_id) references auth_user;
+
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','attendance_failure_information', TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','session_summary', TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','session_response', TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','session_concerns', TRUE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','session_feedback_submitted_by_id', FALSE, TRUE);
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data) VALUES ('appointment','session_feedback_submitted_at', FALSE, TRUE);
