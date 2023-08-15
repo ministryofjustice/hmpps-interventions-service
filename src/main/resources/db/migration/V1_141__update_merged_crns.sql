08d51f42b3dfbf2aabf883cff41ccedbcdb58dde
kth13 <kumar.thangavel@gmail.com>
2023-08-15 14:03:43 +0100

    update crns for merged referrals

---
+++ src/main/resources/db/migration/V1_141__update_merged_crns.sql
@@ -0,0 +1,4 @@
+update referral set service_usercrn = 'E536101' where service_usercrn='E545871' and reference_number = 'HB6167ED';
+update referral set service_usercrn = 'M833264' where service_usercrn='V018774' and reference_number = 'LC2279AC';
+update draft_referral set service_usercrn = 'E536101' where id = '83260e0b-bdc8-4f50-a257-93cf51c4e971';
+update draft_referral set service_usercrn = 'M833264' where id = '804b650b-ad90-403a-92d4-8f8caaa05ca1';
