4f3dff4ac58b77efa64b7e6e0d2cf0fa93da8b35
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-09-07 09:27:49 +0100

    Fix further imports for jakarta declarations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/ComplexityLevel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/ComplexityLevel.kt
@@ -1,13 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity
 
-import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import javax.validation.constraints.NotNull
-import org.hibernate.annotations.Type
 import java.util.UUID
 
 @Entity
@@ -15,7 +13,6 @@ data class ComplexityLevel(
   @Id val id: UUID,
   val title: String,
   val description: String,
-  @Type(PostgreSQLEnumType::class)
   @Enumerated(EnumType.STRING)
   @Column(name = "complexity")
   val complexity: Complexity,
