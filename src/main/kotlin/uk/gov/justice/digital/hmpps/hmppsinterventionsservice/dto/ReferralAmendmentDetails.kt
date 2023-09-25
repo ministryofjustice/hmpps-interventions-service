542c55c33830e9aee56dd990f7f6c9729092dc75
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-09-25 15:21:50 +0100

    Fix error due to jackson parsing fail on ReferralAmendmentDetails
    Tag parameter for constructor argument

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/ReferralAmendmentDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/ReferralAmendmentDetails.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ReferralAmendmentDetails(
+  @JsonProperty("values")
   var values: List<String>,
 )
