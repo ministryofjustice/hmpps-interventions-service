0a53705822fafa1b94585a2359cd3f0a5594d109
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-09-25 15:46:42 +0100

    Upgrade and fix ConcludeReferralsReader
    Now using EntityManagerFactory and the new JpaCursorItemReader classes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/concludereferrals/ConcludeReferralsReader.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/concludereferrals/ConcludeReferralsReader.kt
@@ -1,26 +1,28 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jobs.oneoff.concludereferrals
 
+import jakarta.persistence.EntityManagerFactory
 import org.hibernate.SessionFactory
 import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.item.database.HibernateCursorItemReader
+import org.springframework.batch.item.database.JpaCursorItemReader
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
 
 @Component
 @JobScope
 class ConcludeReferralsReader(
-  sessionFactory: SessionFactory,
-) : HibernateCursorItemReader<Referral>() {
+  entityManagerFactory: EntityManagerFactory,
+) : JpaCursorItemReader<Referral>() {
 
   init {
     this.setName("concludeReferralsReader")
-    this.setSessionFactory(sessionFactory)
+    this.setEntityManagerFactory(entityManagerFactory)
     this.setQueryString(
-      "SELECT r FROM Referral r " +
-        "LEFT JOIN EndOfServiceReport e ON r.id = e.referral.id " +
+      "SELECT re FROM Referral re " +
+        "LEFT JOIN EndOfServiceReport e ON re.id = e.referral.id " +
         "WHERE e.id IS null " +
-        "AND end_requested_at IS NOT null " +
-        "AND concluded_at is null",
+        "AND re.endRequestedAt IS NOT null " +
+        "AND re.concludedAt is null",
     )
   }
 }
