564fa87f4cc77d98757d2a35821516ba77771ddc
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-10-06 14:22:14 +0100

    Disable concurrent index build

--- src/main/resources/db/migration/V1_88__eosr_unique_constraint.sql
+++ src/main/resources/db/migration/V1_88__eosr_unique_constraint.sql
@@ -1,2 +1,2 @@
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS ix_unique_eosr_referral
+CREATE UNIQUE INDEX /*CONCURRENTLY*/ IF NOT EXISTS ix_unique_eosr_referral
     ON end_of_service_report (referral_id);
