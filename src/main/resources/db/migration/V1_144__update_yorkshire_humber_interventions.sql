bc66e7b7b5006ac2d61ddce1d57ae60159108014
kth13 <kumar.thangavel@gmail.com>
2023-10-11 10:30:58 +0100

    Amending pcc regions for some of the yorkshire and humberside contracts

---
+++ src/main/resources/db/migration/V1_144__update_yorkshire_humber_interventions.sql
@@ -0,0 +1,3 @@
+update dynamic_framework_contract set pcc_region_id = 'north-yorkshire', nps_region_id = null where contract_reference = 'PRJ_7681';
+update dynamic_framework_contract set pcc_region_id = 'west-yorkshire', nps_region_id = null where contract_reference = 'PRJ_7683';
+update dynamic_framework_contract set pcc_region_id = 'humberside' , nps_region_id = null where contract_reference = 'PRJ_7907';
