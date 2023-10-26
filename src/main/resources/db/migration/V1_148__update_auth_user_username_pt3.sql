db0464de93d100cc454d09b0d425dbf740e3fc46
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2023-10-26 11:24:10 +0100

    update usernames

---
+++ src/main/resources/db/migration/V1_148__update_auth_user_username_pt3.sql
@@ -0,0 +1,4 @@
+update auth_user set user_name = 'NickyLewisNPS' where id = '1500512866';
+update auth_user set user_name = 'wdy26r' where id = '1501605823';
+update auth_user set user_name = 'pda41z' where id = '1501630735';
+update auth_user set user_name = 'pbm99r' where id = '1501617381';
