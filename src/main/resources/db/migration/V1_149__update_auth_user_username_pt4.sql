e22bad66a214affe71523081f95c1fedfc90f138
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-11-09 13:12:13 +0000

    Rename an additional user (from SHP)

--- src/main/resources/db/migration/V1_149__update_auth_user_username_pt4.sql
+++ src/main/resources/db/migration/V1_149__update_auth_user_username_pt4.sql
@@ -1,2 +1,3 @@
 update auth_user set user_name = 'EmilyRichardsonNPS' where id = '1501201927';
 update auth_user set user_name = 'its48e' where id = '1501617381';
+update auth_user set user_name = 'ejr75c' where id = '1501552435';
