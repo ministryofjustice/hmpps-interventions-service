6d3447027dbc72bbdcbd28b38476d21bfa38129b
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-12-11 14:23:43 +0000

    Configure batch with a null job repository that runs without write / commit operation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/BatchConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/BatchConfiguration.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.config
 
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer
+import com.github.marschall.spring.batch.inmemory.NullJobRepository
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.batch.core.launch.support.SimpleJobLauncher
 import org.springframework.batch.core.repository.JobRepository
@@ -8,13 +8,12 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
-import javax.sql.DataSource
 
 @Configuration
 class BatchConfiguration(
   @Value("\${spring.batch.concurrency.pool-size}") private val poolSize: Int,
   @Value("\${spring.batch.concurrency.queue-size}") private val queueSize: Int,
-) : DefaultBatchConfigurer() {
+) {
   @Bean
   fun asyncJobLauncher(jobRepository: JobRepository): JobLauncher {
     val taskExecutor = ThreadPoolTaskExecutor()
@@ -23,14 +22,9 @@ class BatchConfiguration(
     taskExecutor.afterPropertiesSet()
 
     val launcher = SimpleJobLauncher()
-    launcher.setJobRepository(jobRepository)
+    launcher.setJobRepository(NullJobRepository()) // override job repo with a read only version
     launcher.setTaskExecutor(taskExecutor)
     launcher.afterPropertiesSet()
     return launcher
-  }
-
-  override fun setDataSource(dataSource: DataSource?) {
-    // override to do not set datasource even if a datasource exist.
-    // initialize will use a Map based JobRepository (instead of database)
   }
 }
