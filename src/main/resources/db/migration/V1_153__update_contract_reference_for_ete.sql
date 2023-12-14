36312f40f5908f9a9bba65f9e7a07a3bf445e7f4
kth13 <kumar.thangavel@gmail.com>
2023-12-14 11:50:14 +0000

    contract offboarding for ETE contracts

---
+++ src/main/resources/db/migration/V1_153__update_contract_reference_for_ete.sql
@@ -0,0 +1,2 @@
+update dynamic_framework_contract set referral_end_at = '2023-12-31 23:59:00+00'
+where contract_reference in ('PRJ_6310', 'PRJ_5462','PRJ_5471','PRJ_5468','PRJ_5428','PRJ_5430','PRJ_5455','PRJ_5466','PRJ_5470','PRJ_5467','PRJ_5469');
