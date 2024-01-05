4a68464b8d9ffe5340e8229b44aee8c3e370b477
kth13 <kumar.thangavel@gmail.com>
2024-01-05 15:45:21 +0000

    Revert "Revert "using native queries to retrieve data for performance reporting.""
    This reverts commit 2f91db05ff5d09a0c7fea2f4189047dfd7b2ec9c.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/ReferralsProcessorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/ReferralsProcessorTest.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ndmis
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ndmis.performance.NdmisDateTime
@@ -34,6 +33,7 @@ internal class ReferralsProcessorTest {
 
     val actionPlanFirst = actionPlanFactory.createApproved(submittedAt = apSubmittedAt, approvedAt = apApprovedAt)
     val actionPlanSecond = actionPlanFactory.createApproved(submittedAt = apSubmittedAt.plusDays(1), approvedAt = apApprovedAt.plusDays(1))
+
     val referral = referralFactory.createEnded(
       sentAt = sentAt,
       concludedAt = concludedAt,
@@ -42,8 +42,7 @@ internal class ReferralsProcessorTest {
       actionPlans = mutableListOf(actionPlanFirst, actionPlanSecond),
     )
 
-    whenever(actionPlanService.getAllAttendedAppointments(anyOrNull())).thenReturn(listOf(appointmentFactory.create()))
-
+    whenever(actionPlanService.getAllAttendedAppointments(actionPlanSecond)).thenReturn(listOf(appointmentFactory.create()))
     val result = processor.process(referral)!!
 
     assertThat(result.referralReference).isEqualTo(referral.referenceNumber)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/serviceprovider/PerformanceReportDataProcessorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/serviceprovider/PerformanceReportDataProcessorTest.kt
@@ -2,26 +2,33 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.service
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Attended
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.repository.AppointmentRepository
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.repository.EndOfServiceReportRepository
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.serviceprovider.performance.PerformanceReportProcessor
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.serviceprovider.performance.model.PerformanceReportReferral
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.ActionPlanService
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ActionPlanFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.AppointmentFactory
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.EndOfServiceReportFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ReferralFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.SupplierAssessmentFactory
 import java.time.OffsetDateTime
+import java.util.Optional
 
 internal class PerformanceReportDataProcessorTest {
   private val actionPlanService = mock<ActionPlanService>()
   private val appointmentFactory = AppointmentFactory()
   private val supplierAssessmentFactory = SupplierAssessmentFactory()
   private val appointmentRepository: AppointmentRepository = mock()
+  private val endOfServiceReportRepository: EndOfServiceReportRepository = mock()
   private val actionPlanFactory = ActionPlanFactory()
+  private val endOfServiceReportFactory = EndOfServiceReportFactory()
 
-  private val processor = PerformanceReportProcessor(actionPlanService)
+  private val processor = PerformanceReportProcessor(actionPlanService, endOfServiceReportRepository)
 
   private val referralFactory = ReferralFactory()
 
@@ -34,13 +41,41 @@ internal class PerformanceReportDataProcessorTest {
     val supplierAssessment = supplierAssessmentFactory.create(appointment = supplierAssessmentFirstAppointment)
     supplierAssessment.appointments.add(supplierAssessmentNewAppointment)
     val actionPlan = actionPlanFactory.createApproved()
+    val endOfServiceReport = endOfServiceReportFactory.create()
     val referral = referralFactory.createSent(actionPlans = mutableListOf(actionPlan), supplierAssessment = supplierAssessment)
 
-    whenever(appointmentRepository.findAllByReferralId(referral.id)).thenReturn(listOf(supplierAssessmentFirstAppointment, supplierAssessmentNewAppointment))
-    whenever(actionPlanService.getFirstAttendedAppointment(actionPlan)).thenReturn(approvedActionPlanAppointment)
-    whenever(actionPlanService.getAllAttendedAppointments(actionPlan)).thenReturn(listOf(unApprovedAppointment, approvedActionPlanAppointment))
+    val performanceReportReferral = PerformanceReportReferral(
+      referralReference = referral.referenceNumber!!,
+      referralId = referral.id,
+      contractReference = referral.intervention.dynamicFrameworkContract.contractReference,
+      organisationId = referral.intervention.dynamicFrameworkContract.primeProvider.id,
+      currentAssigneeEmail = "a.b@xyz.com",
+      serviceUserCRN = referral.serviceUserCRN,
+      dateReferralReceived = referral.sentAt!!,
+      dateSupplierAssessmentFirstArranged = supplierAssessmentFirstAppointment.createdAt,
+      dateSupplierAssessmentFirstScheduledFor = supplierAssessmentFirstAppointment.appointmentTime,
+      dateSupplierAssessmentFirstNotAttended = supplierAssessmentFirstAppointment.appointmentTime,
+      dateSupplierAssessmentFirstAttended = supplierAssessmentNewAppointment.appointmentTime,
+      supplierAssessmentAttendedOnTime = true,
+      firstActionPlanSubmittedAt = unApprovedAppointment.attendanceSubmittedAt,
+      firstActionPlanApprovedAt = actionPlan.approvedAt,
+      approvedActionPlanId = actionPlan.id,
+      numberOfOutcomes = 2,
+      endOfServiceReportId = null,
+      numberOfSessions = 2,
+      endRequestedAt = OffsetDateTime.now(),
+      endRequestedReason = "reason",
+      eosrSubmittedAt = OffsetDateTime.now(),
+      concludedAt = referral.concludedAt,
 
-    val performanceReportData = processor.process(referral)
+    )
+
+    whenever(appointmentRepository.findAllByReferralId(referral.id)).thenReturn(listOf(supplierAssessmentFirstAppointment, supplierAssessmentNewAppointment))
+    whenever(endOfServiceReportRepository.findById(anyOrNull())).thenReturn(Optional.of(endOfServiceReport))
+    whenever(actionPlanService.getFirstAttendedAppointment(referral.id)).thenReturn(approvedActionPlanAppointment)
+    whenever(actionPlanService.getAllAttendedAppointments(referral.id)).thenReturn(listOf(unApprovedAppointment, approvedActionPlanAppointment))
+
+    val performanceReportData = processor.process(performanceReportReferral)
 
     assertThat(performanceReportData.referralId).isEqualTo(referral.id)
     assertThat(performanceReportData.dateReferralReceived).isEqualTo(referral.sentAt)
