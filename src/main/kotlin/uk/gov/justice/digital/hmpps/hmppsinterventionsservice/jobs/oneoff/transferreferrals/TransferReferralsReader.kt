1286152d101fcbd24d0007ccfbbccd38ce97fb1a
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2024-02-08 11:49:27 +0000

    Also fix Entity manager definitions in conclude / transfer referrals

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/transferreferrals/TransferReferralsReader.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/transferreferrals/TransferReferralsReader.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jobs.oneoff.transferreferrals
 
-import jakarta.persistence.EntityManagerFactory
+import org.hibernate.SessionFactory
 import org.springframework.batch.core.configuration.annotation.JobScope
-import org.springframework.batch.item.database.JpaCursorItemReader
+import org.springframework.batch.item.database.HibernateCursorItemReader
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
@@ -11,13 +11,13 @@ import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referra
 @JobScope
 class TransferReferralsReader(
   @Value("#{jobParameters['fromContract']}") fromContract: String,
-  entityManagerFactory: EntityManagerFactory,
-) : JpaCursorItemReader<Referral>() {
+  sessionFactory: SessionFactory,
+) : HibernateCursorItemReader<Referral>() {
   private val transferSignalText = "%Authority to transfer case- On behalf of Advance Charity Ltd, I request and give permission for this referral to be transferred to Women in Prison, as of December 2022.%"
 
   init {
     this.setName("transferReferralsReader")
-    this.setEntityManagerFactory(entityManagerFactory)
+    this.setSessionFactory(sessionFactory)
     this.setQueryString(
       "SELECT r FROM Referral r JOIN CaseNote n ON n.referral = r " +
         "WHERE r.intervention.dynamicFrameworkContract.contractReference = :fromContract " +
