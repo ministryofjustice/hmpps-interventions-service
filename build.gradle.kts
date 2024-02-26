86f9c1dbf3be21e537a4a9e7b9e8ee8eecc2f7f0
kth13 <kumar.thangavel@gmail.com>
2024-02-26 12:02:15 +0000

    upgrading to latest dps spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   kotlin("plugin.spring") version "1.9.22"
   id("org.jetbrains.kotlin.plugin.jpa") version "1.9.22"
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
   id("jacoco")
 }
 
