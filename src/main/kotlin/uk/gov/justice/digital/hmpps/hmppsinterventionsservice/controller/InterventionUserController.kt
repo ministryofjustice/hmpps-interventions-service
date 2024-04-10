5a3c23d5ed8ad4d8316035715c82a86a72aacc60
kth13 <kumar.thangavel@gmail.com>
2024-04-10 11:53:51 +0100

    new api to saving any new user

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/controller/InterventionUserController.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.controller
+
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.authorization.UserMapper
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.AuthUserDTO
+
+@RestController
+@PreAuthorize("hasRole('ROLE_PROBATION') or hasRole('ROLE_CRS_PROVIDER') or hasRole('ROLE_INTERVENTIONS_API_READ_ALL')")
+class InterventionUserController(private val userMapper: UserMapper) {
+
+  @PostMapping("/intervention/user")
+  fun createNewUserFromToken(authenticationToken: JwtAuthenticationToken): AuthUserDTO {
+    return AuthUserDTO.from(userMapper.fromToken(authenticationToken))
+  }
+}
