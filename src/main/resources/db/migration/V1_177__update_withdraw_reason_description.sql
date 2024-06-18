ee57e0f11ba58f7d832b7814e9505731d20760ad
kth13 <kumar.thangavel@gmail.com>
2024-06-18 14:56:22 +0100

    update description for some withdrawal reasons

---
+++ src/main/resources/db/migration/V1_177__update_withdraw_reason_description.sql
@@ -0,0 +1,2 @@
+update withdrawal_reason set description = 'Work, caring commitments, or sickness' where code = 'WOR';
+update withdrawal_reason set description = 'Any other change of circumstance' where code = 'ANO';
