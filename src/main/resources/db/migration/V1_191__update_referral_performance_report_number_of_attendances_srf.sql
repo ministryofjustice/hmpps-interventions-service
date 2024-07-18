34c34fb4f8cb3040bd7694615e9d7b013cc44872
Andrew Hodgson <115979893+awhodgson256@users.noreply.github.com>
2024-07-18 08:08:22 +0100

    Fix logic in first attendance and attendance counts to use a.attended = 'LATE' instead of a.late flag. (#1968)

---
+++ src/main/resources/db/migration/V1_191__update_referral_performance_report_number_of_attendances_srf.sql
@@ -0,0 +1,19 @@
+create or replace function performance_report_attendances_count(referral_id UUID)
+  returns integer
+as
+$body$
+SELECT count(*)
+FROM appointment a
+WHERE a.referral_id = $1
+	AND a.stale = false
+	AND (a.superseded is null OR a.superseded = false)
+	AND ((a.did_session_happen is null AND (a.attended = 'LATE' OR a.attended = 'YES'))
+	OR (a.did_session_happen = true AND a.attended = 'YES'))
+	AND NOT EXISTS
+    (
+        SELECT 1
+        FROM supplier_assessment_appointment saa
+        WHERE saa.appointment_id = a.id
+    )
+$body$
+language sql;
