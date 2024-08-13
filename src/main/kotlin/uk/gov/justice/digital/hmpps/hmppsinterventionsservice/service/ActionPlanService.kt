2a528db80162748f4f0b006d1d6ab789bae97d45
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2024-08-13 11:01:02 +0100

    ktlintFormat

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ActionPlanService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ActionPlanService.kt
@@ -87,8 +87,8 @@ class ActionPlanService(
     val savedSubmittedActionPlan = actionPlanRepository.save(submittedActionPlan)
     actionPlanEventPublisher.actionPlanSubmitEvent(savedSubmittedActionPlan)
 
-    //if after 2nd september, auto-approve
-    if(LocalDate.now().isAfter(LocalDate.of(2024,9,1))){
+    // if after 2nd september, auto-approve
+    if (LocalDate.now().isAfter(LocalDate.of(2024, 9, 1))) {
       approveActionPlan(id, AuthUser.interventionsServiceUser)
     }
 
