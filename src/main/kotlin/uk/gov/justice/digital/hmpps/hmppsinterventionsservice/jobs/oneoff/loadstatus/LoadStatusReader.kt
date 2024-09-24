f018ad18a6309d7ab2ae1b9fe8c2367d0025680f
Marco Collura <118893458+marcocolluramoj@users.noreply.github.com>
2024-09-24 12:59:40 +0100

    Reset referral status (#2023)
    * Amend LoadStatusReader to only look for post-ICA referrals
    
    * ktlint formatting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/loadstatus/LoadStatusReader.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jobs/oneoff/loadstatus/LoadStatusReader.kt
@@ -1,21 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jobs.oneoff.loadstatus
 
-import org.hibernate.SessionFactory
+import jakarta.persistence.EntityManagerFactory
 import org.springframework.batch.core.configuration.annotation.JobScope
-import org.springframework.batch.item.database.HibernateCursorItemReader
+import org.springframework.batch.item.database.JpaCursorItemReader
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
 
 @Component
 @JobScope
 class LoadStatusReader(
-  sessionFactory: SessionFactory,
-) : HibernateCursorItemReader<Referral>() {
+  entityManagerFactory: EntityManagerFactory,
+) : JpaCursorItemReader<Referral>() {
   init {
     this.setName("loadStatusReader")
-    this.setSessionFactory(sessionFactory)
+    this.setEntityManagerFactory(entityManagerFactory)
     this.setQueryString(
-      "SELECT r FROM Referral r where r.status is null",
+      "SELECT r FROM Referral r where r.status = 'POST_ICA'",
     )
   }
 }
