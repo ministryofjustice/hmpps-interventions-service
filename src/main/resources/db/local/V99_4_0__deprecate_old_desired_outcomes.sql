bb5619e86ecefc9018a393f9fc78b35fd43ccea6
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-04-08 10:34:23 +0100

    IPB-1327: Add new desired outcomes for all non-accommodation service categories (#2072)
    * IPB-1327: Add local migrations for the non-accommodation desired outcomes changes
    
    * remove dependence and recovery from accomodation changes

--- src/main/resources/db/local/V99_4_0__deprecate_old_desired_outcomes.sql
+++ src/main/resources/db/local/V99_4_0__deprecate_old_desired_outcomes.sql
@@ -14,7 +14,9 @@ VALUES
 -- Un-deprecates new outcomes
 UPDATE desired_outcome
 SET deprecated_at = null
-where id IN ('3ecf4b60-7973-419a-b8c3-ef54b078ef00',
+where id IN (
+    -- Accommodation
+             '3ecf4b60-7973-419a-b8c3-ef54b078ef00',
              '0092ec26-4c2a-4eb9-9b70-687e9936d52b',
              'efe6463f-20da-4ab3-b1b4-74b785be8e5d');
 
@@ -22,6 +24,7 @@ where id IN ('3ecf4b60-7973-419a-b8c3-ef54b078ef00',
 UPDATE desired_outcome
 SET deprecated_at = '2025-04-03 00:00:00.000000 +00:00'
 where id IN (
+    -- Accommodation
              '301ead30-30a4-4c7c-8296-2768abfb59b5',
              '65924ac6-9724-455b-ad30-906936291421',
              '9b30ffad-dfcb-44ce-bdca-0ea49239a21a',
@@ -36,13 +39,16 @@ where id IN (
 -- -- New outcomes
 -- UPDATE desired_outcome
 -- SET deprecated_at = '2025-10-10 00:00:00.000000 +00:00'
--- where id IN ('3ecf4b60-7973-419a-b8c3-ef54b078ef00',
+-- where id IN (
+-- Accommodation
+-- '3ecf4b60-7973-419a-b8c3-ef54b078ef00',
 --              '0092ec26-4c2a-4eb9-9b70-687e9936d52b',
 --              'efe6463f-20da-4ab3-b1b4-74b785be8e5d');
 -- -- Old outcomes
 -- UPDATE desired_outcome
 -- SET deprecated_at = null
 -- where id IN (
+-- Accommodation
 --              '301ead30-30a4-4c7c-8296-2768abfb59b5',
 --              '65924ac6-9724-455b-ad30-906936291421',
 --              '9b30ffad-dfcb-44ce-bdca-0ea49239a21a',
@@ -56,6 +62,8 @@ where id IN (
 -- Uncomment to remove new desired outcomes
 -- DELETE
 -- FROM desired_outcome
--- where id IN ('3ecf4b60-7973-419a-b8c3-ef54b078ef00',
+-- where id IN (
+-- -- Accommodation
+-- '3ecf4b60-7973-419a-b8c3-ef54b078ef00',
 --              '0092ec26-4c2a-4eb9-9b70-687e9936d52b',
 --              'efe6463f-20da-4ab3-b1b4-74b785be8e5d');
