2928dbdda00830b1376212beba06e657aaee90d4
Kumar Thangavel <kumar.thangavel@digital.justice.gov.uk>
2025-04-17 16:27:52 +0100

    updating the description and start date for DNR south west contracts (#2080)

---
+++ src/main/resources/db/migration/V1_215__update_south-west_contracts_description_referral_start_date.sql
@@ -0,0 +1,116 @@
+update intervention set description='Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals via the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in North Somerset, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = 'd9715b0a-ffc6-434c-994e-10dfaf9b3070';
+
+update dynamic_framework_contract set referral_start_date = '2025-04-29', start_date = '2025-04-29' where id='92231bb6-75c7-4d3d-97a4-d05ce00d2d28';
+
+
+update intervention set description='Dorset excluding Bournemouth. Christchurch and Poole. Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals via the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Dorset, excluding Bournemouth. Christchurch and Poole, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = 'd8c8f330-8cb4-4351-919d-7c0d91cf83b7';
+
+update intervention set description='Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals on the SW Intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Bournemouth, Christchurch and Poole, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = 'd60641d8-4416-4dc7-864f-c08d102425b8';
+
+update intervention set description='Devon excluding Plymouth and Torbay. Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals via the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Devon excluding Plymouth and Torbay, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = '4729eb6f-28cc-4e8c-b9bb-a891bbf11681';
+
+update intervention set description='Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals vis the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Wiltshire, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = '6e004a42-137b-4b6d-a9a2-43a09e031da2';
+
+update intervention set description='Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals via the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Swindon, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = 'e1ec981e-3823-468c-8198-470ed5ebb710';
+
+update intervention set description='Dependency services to reduce the harmful effects of substance misuse, promote healthier behaviours, and improve overall well-being. Recovery services focus on helping individuals maintain long-term recovery and rebuild their lives after overcoming dependency. Practitioners are requested to utilise the Substance Misuse Screening Tool to inform referrals via the SW intranet.
+
+                                     These services provide ongoing support through various interventions, such as individual and group therapy, life skills training, and relapse prevention programs and address the physical, psychological, and social aspects of dependency, providing a comprehensive approach to treatment and recovery.
+
+                                     Services are available for all adult males on probation in Gloucestershire, who are not subject to an Alcohol Community Treatment Requirement and/or a Drug Community Treatment Requirement and are either:
+
+                                     - on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated.
+
+                                     - pre-release to people who are convicted. For most cases a pre-release referral will be beneficial to support the person prior to release, by building and maintaining relationships to maximise engagement once released, develop close working relationships with treatment providers and support post release appointments with local substance misuse teams.
+
+                                     - released on licence and post-sentence supervision.
+
+                                     Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable. 
+
+                                     Services are offered as either Low, Medium or High complexity.' where id = '71fff632-dc5a-4a2d-bc80-950f5dbe70e5';
+
+update dynamic_framework_contract set referral_start_date = '2025-06-01', start_date = '2025-06-01' where id='084c3b6f-3950-40db-92c6-22f7d954c148';
