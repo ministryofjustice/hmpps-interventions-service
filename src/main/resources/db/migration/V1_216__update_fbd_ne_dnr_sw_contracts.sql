e8d1863615ccacbc5e9f8e31282a81ed0b0342a7
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2025-04-22 10:50:12 +0100

    updating the description for FBD in North east

---
+++ src/main/resources/db/migration/V1_216__update_fbd_ne_dnr_sw_contracts.sql
@@ -0,0 +1,20 @@
+update intervention set description='Services are available for all adult males on probation either on a Community Sentence or Suspended Sentence Order and subject to a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated, pre-release to people who are sentenced or released on licence and post-sentence supervision.
+
+                                     Services are also available to all adult males pre-release who are sentenced. These services can be delivered at any point during the custodial element of the sentence and can be accessed by all people sentenced, regardless of the location they will be released to. Interventions commenced in custody, but not completed prior to release, may continue in the community until the intervention is completed under the same referral.
+
+                                     Services can be delivered as part of a Rehabilitation Activity Requirement (RAR) whereby a specified number of RAR Activity Days will be allocated by the Probation Service for those subject to a Community Sentence or Suspended Sentence Order. Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable.
+
+                                     Services are offered as Medium or High complexity in community.
+
+                                     Services are offered as High complexity in custody.
+
+                                     A range of delivery methods need to be used including: designing and delivery, support and advocacy and advice, guidance and information.
+
+                                     Interventions must be tailored to meet the specific needs of each Person(s) on Probation to enable them to make progress towards their agreed outcomes, as identified in the Person(s) on Probations Action Plan and aim to secure one or more of the following outcomes:
+
+                                     • The person referred’s financial management skills are developed and/or enhanced, including online banking skills
+                                     • The person referred can successfully navigate the benefits system
+                                     • Pathways are established to help the person referred to maintain and sustain an income, safely manage money and reduce debt
+                                     • The person referred has access to appropriate financial products, advice and/or services
+                                     • The person referred gains quick access to universal credit, including pre-release referrals
+                                     • The person referred is supported to complete tasks that they would not otherwise be able to do whilst being in custody e.g. support with banking or debt management' where id = '5575e6bf-0eac-4da7-be1c-5ec89a9bbc3b';
