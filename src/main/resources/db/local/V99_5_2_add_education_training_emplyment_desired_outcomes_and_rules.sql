3131762e62d41829851eec5520cc31effa095d7b
Jack Dempster <jack.dempster@justice.gov.uk>
2025-04-22 12:15:42 +0100

    add education training and employment migrations

---
+++ src/main/resources/db/local/V99_5_2_add_education_training_emplyment_desired_outcomes_and_rules.sql
@@ -0,0 +1,17 @@
+-- Inserts for Service Pathway: Education, Training and Employment
+INSERT INTO desired_outcome (id,
+                             description,
+                             service_category_id,
+                             deprecated_at)
+VALUES ('b0b27ea1-1ad3-4273-b45d-239f8e6466d3',
+        'Service User increases their employability.',
+        'ca374ac3-84eb-4b91-bea7-9005398f426f',
+        NULL),
+       ('9a7acb60-a634-45d8-bd27-b65b6ec4f185',
+        'Service User commences suitable Education, Training & Employment.',
+        'ca374ac3-84eb-4b91-bea7-9005398f426f',
+        NULL),
+       ('f2913c23-8d8a-4dc4-8b9e-dde5cff87870',
+        'Service User maintains suitable Education, Training & Employment.',
+        'ca374ac3-84eb-4b91-bea7-9005398f426f',
+        NULL);
