44906352b96cb16d11fb5f6f6bf53a111d81c194
nia evans <nia.evans@digital.justice.gov.uk>
2025-04-24 15:09:58 +0100

    Updates to Education Training and Employment script

--- src/main/resources/db/local/V99_5_2_add_education_training_emplyment_desired_outcomes_and_rules.sql
+++ src/main/resources/db/local/V99_5_2_add_education_training_emplyment_desired_outcomes_and_rules.sql
@@ -1,42 +1,41 @@
--- Inserts for Service Pathway: Education, Training and Employment
-INSERT INTO desired_outcome (id,
-                             description,
-                             service_category_id,
-                             deprecated_at)
-VALUES ('b0b27ea1-1ad3-4273-b45d-239f8e6466d3',
-        'Service User increases their employability.',
-        'ca374ac3-84eb-4b91-bea7-9005398f426f',
-        NULL),
-       ('9a7acb60-a634-45d8-bd27-b65b6ec4f185',
-        'Service User commences suitable Education, Training & Employment.',
-        'ca374ac3-84eb-4b91-bea7-9005398f426f',
-        NULL),
-       ('f2913c23-8d8a-4dc4-8b9e-dde5cff87870',
-        'Service User maintains suitable Education, Training & Employment.',
-        'ca374ac3-84eb-4b91-bea7-9005398f426f',
-        NULL);
+-- Service Pathway: Eduction, Training and Employment
 
--- Deprecates old outcomes
+INSERT INTO desired_outcome (id, description, service_category_id, deprecated_at)
+VALUES
+    ('53eff054-63a4-4776-8c99-b17c942c4f6f', 'Service User increases their employability.',
+     'ca374ac3-84eb-4b91-bea7-9005398f426f', '2025-10-10 00:00:00.000000 +00:00'),
+    ('e0d5bf23-b2ad-4753-866a-d76e072b2c09', 'Service User commences suitable Education, Training & Employment.',
+     'ca374ac3-84eb-4b91-bea7-9005398f426f', '2025-10-10 00:00:00.000000 +00:00'),
+    ('804b586c-59cb-4fe0-80a0-34500ea24fc9', 'Service User maintains suitable Education, Training & Employment.',
+     'ca374ac3-84eb-4b91-bea7-9005398f426f', '2025-10-10 00:00:00.000000 +00:00');
+
 UPDATE desired_outcome
 SET deprecated_at = '2025-04-03 00:00:00.000000 +00:00'
-where id IN ('8f438eb1-4f5c-4c23-9436-2ef784fb2426', 'd45e5b8f-7ae3-4fed-8c6b-73959cfc1190',
-             '42077326-ca07-40ea-af20-902249af844e', '0541193e-ba84-46e7-973f-82a9900d7521'
+where id IN (
+             '8f438eb1-4f5c-4c23-9436-2ef784fb2426',
+             'd45e5b8f-7ae3-4fed-8c6b-73959cfc1190',
+             '42077326-ca07-40ea-af20-902249af844e',
+             '0541193e-ba84-46e7-973f-82a9900d7521'
     );
+
 
 -- -- Uncomment to roll back to original state
 -- DELETE
 -- FROM desired_outcome
 -- where id in
 --       (
---        'b0b27ea1-1ad3-4273-b45d-239f8e6466d3',
---        '9a7acb60-a634-45d8-bd27-b65b6ec4f185',
---        'f2913c23-8d8a-4dc4-8b9e-dde5cff87870'
+--        '53eff054-63a4-4776-8c99-b17c942c4f6f',
+--        'e0d5bf23-b2ad-4753-866a-d76e072b2c09',
+--        '804b586c-59cb-4fe0-80a0-34500ea24fc9'
 --           );
 --
 -- UPDATE desired_outcome
 -- SET deprecated_at = null
 -- where id IN
 --       (
---        '8f438eb1-4f5c-4c23-9436-2ef784fb2426', 'd45e5b8f-7ae3-4fed-8c6b-73959cfc1190',
---        '42077326-ca07-40ea-af20-902249af844e', '0541193e-ba84-46e7-973f-82a9900d7521'
+--         '8f438eb1-4f5c-4c23-9436-2ef784fb2426'
+--         'd45e5b8f-7ae3-4fed-8c6b-73959cfc1190',
+--         '42077326-ca07-40ea-af20-902249af844e',
+--         '0541193e-ba84-46e7-973f-82a9900d7521'
 --           );
+
