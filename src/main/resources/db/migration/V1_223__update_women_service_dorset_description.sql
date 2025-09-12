18125460b4c1f72285a181926758d2405cb4afab
Kumar Thangavel <kumar.thangavel@digital.justice.gov.uk>
2025-09-12 12:15:37 +0100

    updating description for women services in dorset (#2138)

---
+++ src/main/resources/db/migration/V1_223__update_women_service_dorset_description.sql
@@ -0,0 +1,60 @@
+update intervention set description=$$A holistic service for women which secure improved outcomes in relation to the following rehabilitative needs:
+
+•        Accommodation
+•        ETE
+•        Dependency and Recovery
+•        Finance, Benefits and Debt
+•        Emotional Wellbeing
+•        Family and Significant Others
+•        Lifestyle and Associates
+•        Social Inclusion, including mentoring
+
+The Women's Specific Service is intended to ensure that the Interventions are responsive to the specific needs and characteristics of Women Service Users. The service will be delivered in an environment which is safe and suitable for women and by staff who are trained in trauma-informed and trauma-responsive approaches and also in women-only group(s) and/or with the option for woman member of the Supplier Personnel.
+
+Services are available for female Service Users, irrespective of risk level or complexity level, on a Community/ Suspended Sentence Order with a RAR or on Licence/Post-Sentence Supervision. The Accommodation and Social Inclusion ‘mentoring’ service will also be delivered pre-release for those who will be released under Probation supervision (to be consistent with accommodation one). Appointments which are part of sentence delivery will be enforceable.
+
+A range of approaches will be used including direct delivery of interventions, support and advocacy, and advice and guidance. Interventions need to be tailored to reflect the complexity of an individual woman’s needs and to recognise and respond to barriers (including child-care commitments, social isolation, religion, language and other cultural barriers, sexual orientation, domestic abuse, involvement with the sex industry, childhood sexual & physical abuse, controlling coercive relationships; substance dependency and lack of self-esteem) to achieving personal change.
+
+Activities:
+
+Accommodation
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service - Community
+
+Open Service - Pre-release
+
+Finance Benefits & Debt
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+ETE
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+Dependency & Recovery
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+Family & Significant Others
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+Lifestyle & Associates
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+Emotional Wellbeing
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service
+
+Social Inclusion
+1-2-1. The number of sessions depends upon complexity levels and categories selected. The action plan is regularly reviewed and updated in consultation with the Responsible Officer, responding dynamically to the woman's changing needs, and additional information she discloses as trust develops.; All of Dorset including Bournemouth, Christchurch and Poole and outreach in rural areas
+
+Open Service - Pre-release$$ where id = 'fd4beacb-cb18-4a24-b5c7-903bb66b8317';
