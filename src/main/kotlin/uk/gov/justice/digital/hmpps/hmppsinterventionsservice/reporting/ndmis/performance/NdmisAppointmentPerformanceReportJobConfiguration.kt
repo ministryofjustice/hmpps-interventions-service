da6aa82e8e88aeb93f0a540cc4e363026a4bab7a
Kumar Thangavel <kumar.thangavel@digital.justice.gov.uk>
2025-11-10 15:12:15 +0000

    fixing the performance of appointment ndmis cron job (#2185)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/performance/NdmisAppointmentPerformanceReportJobConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/performance/NdmisAppointmentPerformanceReportJobConfiguration.kt
@@ -27,13 +27,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.config.S3Bucket
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jobs.scheduled.OnStartupJobLauncherFactory
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Attended
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.BatchUtils
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.NPESkipPolicy
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.OutputPathIncrementer
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.QueryLoader
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ReferralChunkProgressListener
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.TimestampIncrementer
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.S3Service
 import java.nio.file.Path
+import java.time.Instant
+import java.time.OffsetDateTime
+import java.time.ZoneId
 import java.util.UUID
 import javax.sql.DataSource
 
@@ -52,6 +57,7 @@ class NdmisAppointmentPerformanceReportJobConfiguration(
   private val onStartupJobLauncherFactory: OnStartupJobLauncherFactory,
   private val entityManagerFactory: EntityManagerFactory,
   private val dataSource: DataSource,
+  private val queryLoader: QueryLoader,
   @Qualifier("transactionManager") private val transactionManager: PlatformTransactionManager,
   @Value("\${spring.batch.jobs.ndmis.performance-report.chunk-size}") private val chunkSize: Int,
 ) {
@@ -66,24 +72,30 @@ class NdmisAppointmentPerformanceReportJobConfiguration(
 
   @Bean("ndmisAppointmentReader")
   @JobScope
-  fun ndmisReader(): JdbcCursorItemReader<ReferralMetadata> {
-    // this reader returns only referral ID and reference for processing
-    return JdbcCursorItemReaderBuilder<ReferralMetadata>()
-      .name("ndmisAppointmentReportReader")
-      .dataSource(dataSource)
-      .sql("SELECT id, reference_number FROM referral")
-      .rowMapper { rs, _ ->
-        ReferralMetadata(
-          referralId = UUID.fromString(rs.getString("id")),
-          referralReference = rs.getString("reference_number"),
-        )
-      }
-      .build()
-  }
+  fun ndmisReader(): JdbcCursorItemReader<AppointmentData> = JdbcCursorItemReaderBuilder<AppointmentData>()
+    .name("ndmisAppointmentReportReader")
+    .dataSource(dataSource)
+    .sql(this.queryLoader.loadQuery("ndmis-appointment-report.sql"))
+    .rowMapper { rs, _ ->
+      AppointmentData(
+        referralReference = rs.getString("referral_ref"),
+        referralId = UUID.fromString(rs.getString("referral_id")),
+        appointmentId = UUID.fromString(rs.getString("appointmentId")),
+        appointmentTime = NdmisDateTime(rs.getObject("appointment_time", OffsetDateTime::class.java)),
+        durationInMinutes = rs.getInt("duration_in_minutes"),
+        bookedAt = NdmisDateTime(rs.getObject("booked_at", OffsetDateTime::class.java)),
+        attended = rs.getString("attended")?.let { Attended.valueOf(it) },
+        attendanceSubmittedAt = rs.getObject("attendance_submitted_at", OffsetDateTime::class.java)?.let { NdmisDateTime(it) },
+        notifyPPOfAttendanceBehaviour = rs.getObject("notifyppof_attendance_behaviour") as Boolean?,
+        deliusAppointmentId = (rs.getObject("delius_appointment_id") as? Long)?.toString() ?: "",
+        reasonForAppointment = AppointmentReason.valueOf(rs.getString("reason_for_appointment")),
+      )
+    }
+    .build()
 
   @Bean
   @StepScope
-  fun ndmisAppointmentWriter(@Value("#{jobParameters['outputPath']}") outputPath: String): FlatFileItemWriter<Collection<AppointmentData>> = batchUtils.recursiveCollectionCsvFileWriter(
+  fun ndmisAppointmentWriter(@Value("#{jobParameters['outputPath']}") outputPath: String): FlatFileItemWriter<AppointmentData> = batchUtils.csvFileWriter(
     "ndmisAppointmentPerformanceReportWriter",
     FileSystemResource(Path.of(outputPath).resolve(appointmentReportFilename)),
     AppointmentData.headers,
@@ -108,13 +120,11 @@ class NdmisAppointmentPerformanceReportJobConfiguration(
 
   @Bean
   fun ndmisWriteAppointmentToCsvStep(
-    @Qualifier("ndmisAppointmentReader") ndmisReader: JdbcCursorItemReader<ReferralMetadata>,
-    processor: AppointmentProcessor,
-    writer: FlatFileItemWriter<Collection<AppointmentData>>,
+    @Qualifier("ndmisAppointmentReader") ndmisReader: JdbcCursorItemReader<AppointmentData>,
+    writer: FlatFileItemWriter<AppointmentData>,
   ): Step = StepBuilder("ndmisWriteAppointmentToCsvStep", jobRepository)
-    .chunk<ReferralMetadata, List<AppointmentData>>(chunkSize, transactionManager)
+    .chunk<AppointmentData, AppointmentData>(chunkSize, transactionManager)
     .reader(ndmisReader)
-    .processor(processor)
     .writer(writer)
     .faultTolerant()
     .skipPolicy(skipPolicy)
@@ -131,5 +141,15 @@ class NdmisAppointmentPerformanceReportJobConfiguration(
     s3Service.publishFileToS3(ndmisS3Bucket, path, pathPrefix, acl = ObjectCannedACL.BUCKET_OWNER_FULL_CONTROL)
     path.toFile().deleteOnExit()
     RepeatStatus.FINISHED
+  }
+
+  private fun instantToOffsetNotNull(instant: Instant?): OffsetDateTime {
+    val resolved = instant ?: Instant.now()
+    return OffsetDateTime.ofInstant(resolved, ZoneId.systemDefault())
+  }
+
+  private fun instantToOffsetNull(instant: Instant?): OffsetDateTime? {
+    val resolved = instant ?: Instant.now()
+    return instant ?.let { OffsetDateTime.ofInstant(resolved, ZoneId.systemDefault()) }
   }
 }
