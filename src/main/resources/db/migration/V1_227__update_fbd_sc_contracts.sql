0654c6ae214e064a5259ec631b8c7900f13da43d
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2025-12-03 15:27:43 +0000

    adding flyway migration to update the description

---
+++ src/main/resources/db/migration/V1_227__update_fbd_sc_contracts.sql
@@ -0,0 +1,84 @@
+update intervention set description='Services are available for all adult males on probation and within specified prison establishments.
+
+CRS Finance Benefit & Debt services are available for those on probation who are subject to:
+
+    - a Community Sentence or Suspended Sentence Order and are subject to a Rehabilitation Activity Requirement (RAR)
+    - released on licence and post-sentence supervision.
+
+CRS Finance Benefit & Debt services are available for those in prison who are.
+
+    - Sentenced
+
+Services will include a range of delivery methods including designing and delivery, support, advocacy and advice, and guidance and information. The level of the recipient’s complexity will determine the support required which includes:
+
+    - Community: Low, Medium, and High complexity
+    - Custody: High complexity only
+
+
+For those on probation in the community, a specified number of RAR Activity Days will be allocated. Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment is enforceable.
+
+The CRS Finance Benefit & Debt service is designed to support People on Probation and in Prison to enable them to make progress towards agreed outcomes. These are identified in their individually tailored Action Plan and aims to secure one or more of the following outcomes:
+
+    - access to appropriate financial products, advice and/or services.
+    - to help maintain and sustain an income, safely manage money and
+    reduce debt.
+    - Develop or enhance financial management skills.
+    - successfully navigate the benefits system, including online banking skills.
+    - gains quick access to universal credit, where applicable.
+
+Activities:
+
+Finance Benefits & Debt
+group/One to one; Andover, Basingstoke, Farnborough/Aldershot, Gosport, Havant, Isle of Wight, Lymington/New Forest, Portsmouth, Southampton, Aylesbury, Bicester, Bracknell, High Wycombe, Milton Keynes/Bletchley, Newbury, Oxford, Reading, Slough, HMP Winchester (HIOW) and HMP Bullingdon (TV)
+• Banking
+• Benefits
+• Benefits calculators
+• Budgeting
+• Debt
+• Financial awareness
+• Loans and sanctions
+• My budget
+• Quantifying debt and type of debt' where id = '100a8dfc-f328-40eb-b750-d37c06c0e7f9';
+
+update intervention set description='Services are available for all adult males on probation and within specified prison establishments.
+CRS Finance Benefit & Debt services are available for those on probation who are subject to:
+
+    - a Community Sentence or Suspended Sentence Order and are subject to
+a Rehabilitation Activity Requirement (RAR)
+    - released on licence and post-sentence supervision.
+
+CRS Finance Benefit & Debt services are available for those in prison who are.
+
+    - Sentenced
+
+Services will include a range of delivery methods including designing and delivery, support, advocacy and advice, and guidance and information. The level of the recipient’s complexity will determine the support required which includes:
+
+    - Community: Low, Medium, and High complexity
+    - Custody: High complexity only
+
+For those on probation in the community, a specified number of RAR Activity Days will be allocated. Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment is enforceable.
+
+The CRS Finance Benefit & Debt service is designed to support People on Probation and in Prison to enable them to make progress towards agreed outcomes. These are identified in their individually tailored Action Plan and aims to secure one or more of the following outcomes:
+
+    - access to appropriate financial products, advice and/or services.
+    - to help maintain and sustain an income, safely manage money and
+    reduce debt.
+    - Develop or enhance financial management skills.
+    - successfully navigate the benefits system, including online banking skills.
+    - gains quick access to universal credit, where applicable.
+
+Activities:
+
+Finance Benefits & Debt
+Group/One to one; Andover, Basingstoke, Farnborough/Aldershot, Gosport, Havant, Isle of Wight, Lymington/New Forest, Portsmouth, Southampton, Aylesbury, Bicester, Bracknell, High Wycombe, Milton Keynes/Bletchley, Newbury, Oxford, Reading, Slough, HMP Winchester (HIOW) and HMP Bullingdon (TV)
+• Banking
+• Benefit calculators
+• Benefits
+• Budgeting
+• Debt
+• Financial awareness
+• Loans and sanctions
+• Looking at loans and sanctions
+• Looking at my budget
+• My budget
+• Quantifying debt and type of debt' where id = 'c706771a-9561-4022-8a67-515265742362';
