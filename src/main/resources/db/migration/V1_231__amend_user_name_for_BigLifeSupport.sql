758812011fdbbb31c882087321e67b27bd738ad8
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-01-16 10:48:53 +0000

    update user names from foundation 92 to big life support

---
+++ src/main/resources/db/migration/V1_231__amend_user_name_for_BigLifeSupport.sql
@@ -0,0 +1,3 @@
+update auth_user set user_name='Rene.murabukirwa@blg.cjsm.net' where id='130d59eb-3985-482e-8e39-b5c26a01d558';
+update auth_user set user_name='Skye.cheetham@blg.cjsm.net' where id='5e1b5891-c507-4979-b18c-886f6ca44c54';
+update auth_user set user_name='leilah.batamuliza@blg.cjsm.net' where id='10e008ae-6ec5-4c0d-a3ab-432afaa44200';
