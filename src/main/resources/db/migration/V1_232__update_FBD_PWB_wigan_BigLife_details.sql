6ed1385a18ff74eb20bb01406098a7ae769732d2
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-04-16 13:30:12 +0100

    Update FBD and PWB Big life wigan details

---
+++ src/main/resources/db/migration/V1_232__update_FBD_PWB_wigan_BigLife_details.sql
@@ -0,0 +1,61 @@
+update intervention set incoming_referral_distribution_email='lee.waudby@rbwh.cjsm.net' and description='This is a Big Life Group service delivered by Rebuild With Hope in Wigan.
+
+Services are available for all adult male and female People on Probation, irrespective of risk level or complexity level, on a Community/Suspended Sentence Order with a RAR or on Licence/Post-Sentence Supervision. Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable.
+
+A range of delivery methods will be used including: Support, advocacy, guidance and information.
+
+Interventions will be tailored to meet the specific needs of each Person(s) on Probation to enable them to make progress towards their agreed outcomes, as identified in the Person(s) on Probations Action Plan and aim to secure one or more of the following outcomes:
+• Person(s) on probation has access to appropriate financial products, advice and/or services.
+• Pathways are established to help person(s) on probation maintain and sustain an income, safely manage money and reduce debt.
+• Person(s) on probation financial management skills are developed and/or enhanced.
+• Person(s) on probation can successfully navigate the benefits system, including online banking skills.
+
+Activities:
+
+Finance Benefits & Debt
+1:1; Wigan
+• Individual sessions delivered on a 1:1 basis covering benefits and better of calculations. Advocacy in lodging appeals and advice on how to avoid incurring sanctions.
+• Individual sessions delivered on a 1:1 basis covering budgeting, debt management, advice and guidance
+• Support and advocacy to open bank accounts and access to financial services and adhere to a debt management plan. 1:1 support to support individual with financial literacy.' where id = '9c8ef076-52d1-4133-8fae-e02e6a61d1ed'
+
+update intervention set incoming_referral_distribution_email='lee.waudby@rbwh.cjsm.net' and description='This is a Big Life Group service delivered by Rebuild With Hope in Wigan.
+
+The personal wellbeing service is a holistic service which addresses the following rehabilitative needs: Emotional Wellbeing, Family and Significant Others, Lifestyle and Associates, Social Inclusion, low level accommodation, support with addressing trauma and support for veterans.
+
+Services are available for all adult male and female People on Probation, irrespective of risk level or complexity level, on a Community/Suspended Sentence Order with a RAR or on Licence/Post-Sentence. Any Sessions delivered as part of a RAR Activity Day or as a mandated Licence or Post Sentence Supervision appointment will be Enforceable.
+
+Interventions will be tailored to reflect the complexity of needs and should aim to secure one or more of the following outcomes for People on Probation:
+• Sustained engagement in pro-social leisure interests and purposeful activities
+• Reduced engagement with pro-criminal associates and activities
+• Support to develop or maintain positive family relationships
+• Development of coping skills and strategies to regulate mood and demonstrate perspective-taking and self-care by avoiding risky situations.
+• Develop the ability to access and engage with mental health and or drug and alcohol services
+• Develop ability to recognise and manage triggers to worsening wellbeing.
+• Develop ability to build and maintain appropriate social interactions.
+•  Support to comply with/access any medication/treatment/therapy programmes that will enhance general health and wellbeing
+
+Activities:
+
+Emotional Wellbeing
+1:1 and group sessions; Wigan
+• Group and 1:1 trauma informed sessions for emotional regulation and decision making. Group sessions are delivered on an evolving timetable and include activities such as art therapy and outdoor activities.
+
+
+Lifestyle & Associates
+1:1 and group sessions; Wigan
+• 1:1 and group sessions delivered on evolving timetables. Activities include household management, low level employment advice and guidance, signposting to relevant agencies and applicable courses.
+
+
+Social Inclusion
+1:1 and group sessions; Wigan
+• Social groups and activities run on a changing timetable to include art groups, nature walks, games leagues, drop in sessions to access food banks and digital exclusion groups.
+
+
+Family & Significant Others
+
+1:1 and group sessions; Wigan
+• Provide information to educate families about probation order/license conditions.
+– Offer basic mediation to address familial and relationship issues.
+– Facilitate referrals to mediation services for complex family situations.
+– Offer guidance to people on probation wishing to maintain contact with their children.
+– Facilitate communication with statutory services (such as Children Services).' where id = '90fd86f9-b0f4-496f-8082-9a44431b1f27'
